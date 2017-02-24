fcdf83c7b502a47f96f4652da75f692180615b13
Igor Minar <iminar@google.com>
2017-02-24 07:15:26 +0000

    2da3844 build(language-service): support deep import bundling of language services (#14689)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-beta.8-41da599
+   * @license Angular v4.0.0-beta.8-2da3844
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
