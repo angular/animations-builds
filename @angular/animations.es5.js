9269130753fe4237610d32f173f0416f04279657
Igor Minar <iminar@google.com>
2017-02-24 17:57:53 +0000

    93ddd38 refactor(animations): make modules and bundles into plural Animations (#14695)

--- @angular/animations.es5.js
+++ @angular/animations.es5.js
@@ -1,5 +1,5 @@
 var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}/**
- * @license Angular v4.0.0-beta.8-32c2fd5
+ * @license Angular v4.0.0-beta.8-93ddd38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  *//**
