cc925f968d33016a7fef6deafe98d7d448f7d2e6
GitHub <noreply@github.com>
2017-02-26 04:38:53 +0000

    213e210 docs: changelog - add anchor to the FESM section

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-rc.1-c868457
+   * @license Angular v4.0.0-rc.1-213e210
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
