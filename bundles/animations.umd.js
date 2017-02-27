9ae32ddc6192658eab1caf05e4379dd889606129
Igor Minar <iminar@google.com>
2017-02-27 19:46:47 +0000

    77682a3 fix(compiler): quote `animation` in `RendererTypeV2` and skip if empty (#14773)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-rc.1-213e210
+   * @license Angular v4.0.0-rc.1-77682a3
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
