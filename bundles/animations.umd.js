b1ddbc04e5bfbd775b8534378e4ae6c2d2554ff4
Igor Minar <iminar@google.com>
2017-02-27 23:11:54 +0000

    5caab71 refactor: make identifiers for generated code small to improve dev size

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-rc.1-f356041
+   * @license Angular v4.0.0-rc.1-5caab71
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
