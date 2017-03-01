da3fe926847a97bfb5920fa66a42f3aaffab3bb8
Igor Minar <iminar@google.com>
2017-03-01 21:54:38 +0000

    6bae737 fix(language-service): tolerate errors in decorators (#14634)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-rc.1-7a66a41
+   * @license Angular v4.0.0-rc.1-6bae737
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
