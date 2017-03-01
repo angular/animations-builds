1385e5e665e68bd23c11bed0a8cef13a0888decb
Igor Minar <iminar@google.com>
2017-03-01 21:43:22 +0000

    7a66a41 test(language-service): test `@angular/language-service` can be loaded by tsserver.js (#14721)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-rc.1-79fc1e3
+   * @license Angular v4.0.0-rc.1-7a66a41
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
