1dd7e2f83878af89d30c6383446541bfdff506e5
Igor Minar <iminar@google.com>
2017-03-01 20:37:05 +0000

    79fc1e3 fix(compiler): apply element bindings before host bindings (#14823)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-rc.1-9402df9
+   * @license Angular v4.0.0-rc.1-79fc1e3
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
