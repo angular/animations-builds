e7614f004d16de559fb4e5d294577bdde212dc97
Igor Minar <iminar@google.com>
2017-03-01 19:38:18 +0000

    9402df9 test(platform-server): enable meta tag and title service specs for server (#14574)

--- @angular/animations.es5.js
+++ @angular/animations.es5.js
@@ -1,5 +1,5 @@
 var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}/**
- * @license Angular v4.0.0-rc.1-47bdc2b
+ * @license Angular v4.0.0-rc.1-9402df9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  *//**
--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-rc.1-47bdc2b
+   * @license Angular v4.0.0-rc.1-9402df9
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
