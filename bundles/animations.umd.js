85961795561e030a8b84e9302d66cb661e4320e0
Igor Minar <iminar@google.com>
2017-03-07 07:05:42 +0000

    ebd4463 fix(aio): reimplement the top menu dropping into the side bar

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -43,7 +43,7 @@
   }();
 
   /**
-   * @license Angular v4.0.0-rc.2-b7e76cc
+   * @license Angular v4.0.0-rc.2-ebd4463
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
