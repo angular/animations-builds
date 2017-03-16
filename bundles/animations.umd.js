3e8bd351b670e7635a41992367b47850551d6a7b
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 05:07:06 +0000

    923d0c5 fix: correct UMD resolutions for platform-browser_animations (#15190)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6772c91
+ * @license Angular v4.0.0-rc.3-923d0c5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, function (exports) { 'use strict';
 
     /**
-     * @license Angular v4.0.0-rc.3-6772c91
+     * @license Angular v4.0.0-rc.3-923d0c5
      * (c) 2010-2017 Google, Inc. https://angular.io/
      * License: MIT
      */
