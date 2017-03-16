206f8ae242afe2c9601e6fa61f78a70a5ba4e471
Igor Minar <iminar@google.com>
2017-03-16 06:50:02 +0000

    480a407 build(aio): implement prerendering

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-923d0c5
+ * @license Angular v4.0.0-rc.3-480a407
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, function (exports) { 'use strict';
 
     /**
-     * @license Angular v4.0.0-rc.3-923d0c5
+     * @license Angular v4.0.0-rc.3-480a407
      * (c) 2010-2017 Google, Inc. https://angular.io/
      * License: MIT
      */
