ae417f2f21fee283373df6f17cd84515994a0727
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 17:15:46 +0000

    f40f915 build(aio): upgrade to rxjs@5.2.0

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-11d5309
+ * @license Angular v4.0.0-rc.5-f40f915
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-rc.5-11d5309
+ * @license Angular v4.0.0-rc.5-f40f915
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
