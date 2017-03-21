6e07819178d4dadd4c43da29037a66e389711aa2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 11:50:15 +0000

    1b9c809 refactor(aio): removed dead code

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-b7ba331
+ * @license Angular v4.0.0-rc.5-1b9c809
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-rc.5-b7ba331
+ * @license Angular v4.0.0-rc.5-1b9c809
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
