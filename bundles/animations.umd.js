d111398d2bfdc7792240abc6fefbab39fcf73926
Igor Minar <iminar@google.com>
2017-03-22 20:44:18 +0000

    941f194 docs: spelling errors (#15375)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-8b4edcc
+ * @license Angular v4.0.0-rc.5-941f194
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-rc.5-8b4edcc
+ * @license Angular v4.0.0-rc.5-941f194
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
