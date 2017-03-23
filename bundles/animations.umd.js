78d109925a2199569ef9b5e2bec6d7ddbb8f3560
Victor Berchet <victor@suumit.com>
2017-03-23 17:43:54 +0000

    376088d build(aio): delete content folder before doc-gen (#15414)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-73808dd
+ * @license Angular v4.0.0-rc.6-376088d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-rc.6-73808dd
+ * @license Angular v4.0.0-rc.6-376088d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
