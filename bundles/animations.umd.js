975cc293bcfb6dd66a558c804846b372f63bcf70
GitHub <noreply@github.com>
2017-03-24 02:41:34 +0000

    e1b09e3 docs: update CONTRIBUTING link in README.md

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b2b1195
+ * @license Angular v4.0.0-e1b09e3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-b2b1195
+ * @license Angular v4.0.0-e1b09e3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
