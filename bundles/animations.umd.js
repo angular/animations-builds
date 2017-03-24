2ac39db7aff68d099b52af42c77358485ef64fc1
Victor Berchet <victor@suumit.com>
2017-03-24 00:22:48 +0000

    7a715b2 docs: add changelog for 4.0.0

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-b800a0c
+ * @license Angular v4.0.0-7a715b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-rc.6-b800a0c
+ * @license Angular v4.0.0-7a715b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
