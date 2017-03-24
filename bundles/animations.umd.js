be114002b1ba0cc11b8390ba39e38e99544d1b37
Victor Berchet <victor@suumit.com>
2017-03-24 21:54:35 +0000

    8785b2b fix(router): shouldn't execute CanLoad when a route has been loaded

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-ea848f7
+ * @license Angular v4.0.0-8785b2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-ea848f7
+ * @license Angular v4.0.0-8785b2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
