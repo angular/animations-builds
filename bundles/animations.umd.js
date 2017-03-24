cea2811b1e3838c7f4fbb80e85ee032b179b2b7d
GitHub <noreply@github.com>
2017-03-24 00:36:50 +0000

    db0dca3 docs: add a note about exclusion of symbols prefixed with ɵ from our public api surface (#15440)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-7a715b2
+ * @license Angular v4.0.0-db0dca3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-7a715b2
+ * @license Angular v4.0.0-db0dca3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
