3b5722a314188540eef8dc949ae6aeabf3456cd6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 07:42:35 +0000

    b283038 build(aio): upgrade @angular/service-worker to v1.0.0-beta.8

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-afb17a5
+ * @license Angular v4.0.0-b283038
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-afb17a5
+ * @license Angular v4.0.0-b283038
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
