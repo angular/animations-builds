93bb807f0d04f242839b635141d939e461a9985a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 09:30:24 +0000

    52ea193 build(aio): left align code regions

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
