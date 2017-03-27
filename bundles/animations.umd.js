a2f12c7a1770018264e5522889873f2a5a8c2b3c
Victor Berchet <victor@suumit.com>
2017-03-27 19:12:48 +0000

    15662ef build(aio): update project config for @angular/cli v1.0.0

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-816b389
+ * @license Angular v4.0.0-15662ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-816b389
+ * @license Angular v4.0.0-15662ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
