e7c5e5d71b3d4b7f046b8ec0063a4816dd9ee4c4
Victor Berchet <victor@suumit.com>
2017-03-28 20:49:48 +0000

    d438b88 fix(compiler): ignore errors when evaluating base classes (#15560)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-6269d28
+ * @license Angular v4.0.0-d438b88
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-6269d28
+ * @license Angular v4.0.0-d438b88
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
