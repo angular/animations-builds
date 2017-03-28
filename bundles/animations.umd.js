6c074afafdf5479eed8e55c3da11d2809f654a90
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-28 09:40:45 +0000

    fd72fad docs(aio): update migrated content from anguar.io

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-920bf37
+ * @license Angular v4.0.0-fd72fad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-920bf37
+ * @license Angular v4.0.0-fd72fad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
