22371e7f881dbdd4f6e4c16837e1d96b0501a03b
Victor Berchet <victor@suumit.com>
2017-03-28 23:07:01 +0000

    f368381 fix(router): should run CanActivate after CanDeactivate guards

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-93d48f1
+ * @license Angular v4.0.0-f368381
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-93d48f1
+ * @license Angular v4.0.0-f368381
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
