546912f9df8b353433786d4dd263c36d10e2eee8
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-28 10:07:37 +0000

    9e0d7be build(aio): fix angular-cli config typo

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-fd72fad
+ * @license Angular v4.0.0-9e0d7be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-fd72fad
+ * @license Angular v4.0.0-9e0d7be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
