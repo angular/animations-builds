448cf051a2e721f2aec98d80c2532a24b9eb38a0
Victor Berchet <victor@suumit.com>
2017-03-28 17:20:12 +0000

    a5c972a fix(language-service): be resilient to invalidate ordering (#15470)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-8e03f65
+ * @license Angular v4.0.0-a5c972a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-8e03f65
+ * @license Angular v4.0.0-a5c972a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
