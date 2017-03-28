a8e3dd721277f7ac8a0d86e86e8c54eb586e28a5
Victor Berchet <victor@suumit.com>
2017-03-28 21:58:21 +0000

    861953c feat(aio): add LiveExampleComponent (#15544)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-aa16ccd
+ * @license Angular v4.0.0-861953c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.0.0-aa16ccd
+ * @license Angular v4.0.0-861953c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
