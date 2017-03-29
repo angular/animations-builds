3a6ba9f4740c60963c6771bee6da3dec1588f836
Victor Berchet <victor@suumit.com>
2017-03-29 17:46:33 +0000

    9c77a7c fix(compiler): throw when a component defines both template and templateUrl (#15572)

--- @angular/animations/browser.es5.js
+++ @angular/animations/browser.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-331b9f6
+ * @license Angular v4.0.0-9c77a7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
