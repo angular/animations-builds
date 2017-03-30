b3206f991f04de6576d0f17cb8e482f0dc378db7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-30 22:07:41 +0000

    8ef621a fix(compiler): fix inheritance for AOT with summaries (#15583)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-9e883f5
+ * @license Angular v4.1.0-beta.0-8ef621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-9e883f5
+ * @license Angular v4.1.0-beta.0-8ef621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
