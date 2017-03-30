a69682646d34752ca2191706096576c78a2a6f6a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-30 22:15:15 +0000

    28bf222 feat(aio): add footer links with GoogleFeedbackService (#15605)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8ef621a
+ * @license Angular v4.1.0-beta.0-28bf222
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-8ef621a
+ * @license Angular v4.1.0-beta.0-28bf222
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
