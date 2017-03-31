3431cafe4b20cc9a020f63af3b519d03d47a300d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 15:37:58 +0000

    46f8a6d docs(aio): remove unnecessary blank lines

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-0aa90c6
+ * @license Angular v4.1.0-beta.0-46f8a6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-0aa90c6
+ * @license Angular v4.1.0-beta.0-46f8a6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
