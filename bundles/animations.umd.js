83ed0d6abc006939325ef76ed6541419fb59f95a
Hans <hans@hansl.ca>
2017-04-11 21:41:17 +0000

    0799f18 fix(aio): v2 - when browser lacks service worker, disable sw feature

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-91b2e39
+ * @license Angular v4.1.0-beta.0-0799f18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-91b2e39
+ * @license Angular v4.1.0-beta.0-0799f18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
