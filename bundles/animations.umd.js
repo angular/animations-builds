d3e8076631a5fa82fe653b58299d7ab0f2f86528
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:17:49 +0000

    0614754 docs(aio): final migration from old site

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
