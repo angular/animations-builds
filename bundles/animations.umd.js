4f2a369fbc4a79bf5cf2c35d74a2f552e25b28d9
GitHub <noreply@github.com>
2017-04-12 19:40:35 +0000

    4a052cd build(aio): upgrade to @angular/material#2.0.0-beta.3 (#15606)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-28c2a71
+ * @license Angular v4.1.0-beta.0-4a052cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-28c2a71
+ * @license Angular v4.1.0-beta.0-4a052cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
