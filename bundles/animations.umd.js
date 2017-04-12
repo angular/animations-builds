91f6f866f0118e657a21364345a28bd5bf33167e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 13:47:53 +0000

    2798c1b feat(aio): code block styles with header

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-38d75d4
+ * @license Angular v4.1.0-beta.0-2798c1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-38d75d4
+ * @license Angular v4.1.0-beta.0-2798c1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
