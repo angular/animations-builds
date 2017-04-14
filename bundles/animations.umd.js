13e57ce73d52a1a804f280f94aa00185e6e76c4c
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:17:56 +0000

    ce47d33 fix(compiler): ignore calls to unresolved symbols in metadata (#15970)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-2e47a0d
+ * @license Angular v4.1.0-beta.1-ce47d33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-2e47a0d
+ * @license Angular v4.1.0-beta.1-ce47d33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
