5632611e9aee1b69411c48b9daedbd4cabec672a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:44:19 +0000

    49dfc9f feat(aio): scroll bar styling

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-49d97e1
+ * @license Angular v4.1.0-beta.1-49dfc9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-49d97e1
+ * @license Angular v4.1.0-beta.1-49dfc9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
