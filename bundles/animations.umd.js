e319324ccf92211f738bcd149493cb2b9243c42f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:07 +0000

    c0b1bbe feat(aio): add log rotation in preview server

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8479e9e
+ * @license Angular v4.1.0-beta.1-c0b1bbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-8479e9e
+ * @license Angular v4.1.0-beta.1-c0b1bbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
