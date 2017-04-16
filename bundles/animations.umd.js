087da234d488977ff539d59065e656201a1d454d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 10:26:17 +0000

    4a3c66f feat(aio): fixed sidenav, moved footer inside of sidenav container (#16028)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9a50844
+ * @license Angular v4.1.0-beta.1-4a3c66f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-9a50844
+ * @license Angular v4.1.0-beta.1-4a3c66f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
