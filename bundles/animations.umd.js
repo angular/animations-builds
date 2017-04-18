0947577fcf8b50a9cad15841071a54c76a373280
Tobias Bosch <tbosch1009@gmail.com>
2017-04-18 22:37:00 +0000

    ecd0348 build(aio): extract the title from the content if necessary

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5293794
+ * @license Angular v4.1.0-beta.1-ecd0348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-5293794
+ * @license Angular v4.1.0-beta.1-ecd0348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
