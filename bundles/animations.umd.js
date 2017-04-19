896bec22aea048852c07ece8e6365ca9d1eba134
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:47:50 +0000

    9d40ab9 docs(aio): rename ToH to match the guide name

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-691e86c
+ * @license Angular v4.1.0-beta.1-9d40ab9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-691e86c
+ * @license Angular v4.1.0-beta.1-9d40ab9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
