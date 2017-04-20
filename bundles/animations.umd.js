bffe06d8c5262b071ef205055d201476c52bdb38
Miško Hevery <misko@hevery.com>
2017-04-20 04:30:19 +0000

    745731e fix(compiler): make I18NHtmlParser provider AoT-compliant (#15980)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-bfd5f27
+ * @license Angular v4.1.0-beta.1-745731e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-bfd5f27
+ * @license Angular v4.1.0-beta.1-745731e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
