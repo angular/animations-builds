004eab0bdbe8124a0193cf27e50e7da6b6afc94e
Miško Hevery <misko@hevery.com>
2017-04-21 17:26:57 +0000

    062fc4a docs(aio): change hardcoded links to the old site (#16196)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-cf034f7
+ * @license Angular v4.1.0-rc.0-062fc4a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-cf034f7
+ * @license Angular v4.1.0-rc.0-062fc4a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
