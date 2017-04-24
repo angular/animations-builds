13d7e1e83411a5769de3f1b615710d403d0b20c1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 13:37:12 +0000

    54e587a fix(aio): fix URL redirection for API pages

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-4ac5096
+ * @license Angular v4.1.0-rc.0-54e587a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-4ac5096
+ * @license Angular v4.1.0-rc.0-54e587a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
