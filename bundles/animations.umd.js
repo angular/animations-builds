3e74f751c0b7602e7f51d460cdd8c60e2264f6fe
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 08:41:23 +0000

    d8e2829 feat(aio): boilerplate:add cleans (removes) before adding

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-ac5e6ba
+ * @license Angular v4.1.0-rc.0-d8e2829
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-ac5e6ba
+ * @license Angular v4.1.0-rc.0-d8e2829
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
