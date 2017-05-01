ec644bbf7bdff92286dc08afda2477da5be4d947
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:09:14 +0000

    235eb17 build(aio): improve info-bar template

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ff32c53
+ * @license Angular v4.1.0-235eb17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-ff32c53
+ * @license Angular v4.1.0-235eb17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
