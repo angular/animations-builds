8e3adf4165a60ea8450d47ec17b517da4efdef6a
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:29:30 +0000

    79ed0e7 feat(aio): display API icons in search results

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-04dc248
+ * @license Angular v4.1.0-79ed0e7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-04dc248
+ * @license Angular v4.1.0-79ed0e7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
