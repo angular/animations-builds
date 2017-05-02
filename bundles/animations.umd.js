bacf716ebb9e1b1adc17b95fdcb8a8c8537ae23c
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 00:30:09 +0000

    e263e19 fix(core): don’t stop change detection because of errors

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ac220fc
+ * @license Angular v4.1.0-e263e19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-ac220fc
+ * @license Angular v4.1.0-e263e19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
