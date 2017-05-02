6755bf9086572587b0d48ab7d46ef46b0eaf6f08
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 18:14:16 +0000

    04dc248 feat(aio): comment cleanup, API table styles

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-e263e19
+ * @license Angular v4.1.0-04dc248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-e263e19
+ * @license Angular v4.1.0-04dc248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
