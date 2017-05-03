1d32c74d249ac1eaf8353f7d13ab461e330b4115
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:02:43 +0000

    f29c6bb docs(aio): fix missing title warnings

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
