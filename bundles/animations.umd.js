76f0c8d26bb7bd4bf7a7bb79b921845bd4663b29
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:07:27 +0000

    55b8de9 build(aio): lint examples

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-eb56ab3
+ * @license Angular v4.1.0-55b8de9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-eb56ab3
+ * @license Angular v4.1.0-55b8de9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
