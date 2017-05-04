15da9c5245837afc39d7bbe802d51ca7e105b1df
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 11:46:22 +0000

    895f47a refactor(aio): remove work-around for browsers without ServiceWorker support

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-aec65de
+ * @license Angular v4.1.0-895f47a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.0-aec65de
+ * @license Angular v4.1.0-895f47a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
