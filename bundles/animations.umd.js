4653accfaaf3e3c0c282e5307b075a3bd08c4a5c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:04:48 +0000

    a8379a4 docs(aio): remove toc from all marketing pages

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-b7caa3e
+ * @license Angular v4.2.0-beta.0-a8379a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.0-beta.0-b7caa3e
+ * @license Angular v4.2.0-beta.0-a8379a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
