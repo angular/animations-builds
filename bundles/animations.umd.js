d770ddb45bbe228ac76bc886114c93d75af4d956
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 06:29:29 +0000

    799be9c feat(aio): TOC float right + service refactor

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-566dab1
+ * @license Angular v4.2.0-beta.0-799be9c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.0-beta.0-566dab1
+ * @license Angular v4.2.0-beta.0-799be9c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
