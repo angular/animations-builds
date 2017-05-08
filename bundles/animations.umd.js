0662927e552c83b6e1402976a4e6d075b6e9e47a
Miško Hevery <misko@hevery.com>
2017-05-08 18:06:51 +0000

    98dd609 docs(animations): remove duplicate word (#16508)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-6dc6777
+ * @license Angular v4.2.0-beta.0-98dd609
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.0-beta.0-6dc6777
+ * @license Angular v4.2.0-beta.0-98dd609
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
