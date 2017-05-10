0bfd3e2552b82a8a86d0b7613794d245e2b2c707
Jason Aden <jasonaden@google.com>
2017-05-10 23:52:58 +0000

    62a8618 docs: add changelog for 4.1.2

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.1-e46a65f
+ * @license Angular v4.1.2-62a8618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.1.1-e46a65f
+ * @license Angular v4.1.2-62a8618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
