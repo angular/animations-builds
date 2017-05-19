0dd7fd3014ec828bd6ab9680457dc901358f9021
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 09:00:52 +0000

    a42322d feat(aio): code copy button has tooltip and aria-label

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-b836aca
+ * @license Angular v4.2.0-beta.1-a42322d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.0-beta.1-b836aca
+ * @license Angular v4.2.0-beta.1-a42322d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
