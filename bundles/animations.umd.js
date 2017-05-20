20294ea6abdaf445e059168b06ae231e18c3d660
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-20 11:24:26 +0000

    c8b08f3 feat(aio): implement ScrollSpy service (to highlight the active section in ToC)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-3d382dc
+ * @license Angular v4.2.0-rc.0-c8b08f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.0-rc.0-3d382dc
+ * @license Angular v4.2.0-rc.0-c8b08f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
