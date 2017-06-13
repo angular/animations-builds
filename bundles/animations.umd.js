b7c09a9bf20f284ae800d1d30107d8b6eea1d8c8
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 13:14:53 +0000

    052331f build(aio): add staging environment

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-541c9a9
+ * @license Angular v4.2.2-052331f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.2-541c9a9
+ * @license Angular v4.2.2-052331f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
