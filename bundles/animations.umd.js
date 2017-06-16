cb9def1012ede01f9794d9025cb5f47a3e28691d
Hans Larsen <hans@hansl.ca>
2017-06-16 21:13:23 +0000

    ab81c1c build(aio): update @angular/service-worker to 1.0.0-beta.16

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
