61d00d49e7cfa07e8f4a774c317c587c755f6498
Hans <hans@hansl.ca>
2017-06-19 22:44:22 +0000

    1c6a252 build(aio): upgrade jasmine to v2.6.4

--- @angular/animations/browser/testing.js
+++ @angular/animations/browser/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-2447bd1
+ * @license Angular v4.2.2-1c6a252
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-2447bd1
+ * @license Angular v4.2.2-1c6a252
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.2-2447bd1
+ * @license Angular v4.2.2-1c6a252
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
