d318b2ba3520f3da238029c4abc3ccfe820ebd0f
Hans Larsen <hans@hansl.ca>
2017-06-19 22:12:24 +0000

    09512c7 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3607a48
+ * @license Angular v4.2.3-09512c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.3-3607a48
+ * @license Angular v4.2.3-09512c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
