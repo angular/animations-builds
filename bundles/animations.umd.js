dfc92a943ed61a4d89ca42a98440e8a2b055a107
Hans <hans@hansl.ca>
2017-06-21 23:38:10 +0000

    4352dd2 fix(compiler): avoid emitting self importing factories

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-34cc3f2
+ * @license Angular v4.2.2-4352dd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.2.2-34cc3f2
+ * @license Angular v4.2.2-4352dd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
