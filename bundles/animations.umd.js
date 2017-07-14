beb73ed1eebed2c0011c9bebeff8ea44aba2eab9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 22:26:53 +0000

    55742e4 Revert "revert: revert: ci(aio): exclude changes in aio/content folder"

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-a0b06be
+ * @license Angular v4.3.0-55742e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.3.0-a0b06be
+ * @license Angular v4.3.0-55742e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
