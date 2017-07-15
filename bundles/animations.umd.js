989b5686297c706bae103602e870b875a433258c
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 08:25:27 +0000

    6e2ddcc build(aio): render type parameters of API function exports

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-55742e4
+ * @license Angular v4.3.0-6e2ddcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.3.0-55742e4
+ * @license Angular v4.3.0-6e2ddcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
