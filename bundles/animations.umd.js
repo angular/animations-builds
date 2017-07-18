fbcd6046a4257745d20edb1847c466d221ee27ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:34:24 +0000

    256bc8a docs(http): Make name of injected HttpTestingController consistent

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
