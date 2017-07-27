41dba6c29c11bd394883de12d44261de41b60638
Miško Hevery <misko@hevery.com>
2017-07-27 21:23:44 +0000

    8de2ace docs: add changelog for 4.3.2

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.1-12b8e1a
+ * @license Angular v4.3.2-8de2ace
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.3.1-12b8e1a
+ * @license Angular v4.3.2-8de2ace
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
