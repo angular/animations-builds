0776cacd5ff32e3ba7c05bd61701ddf2af88b316
Victor Berchet <victor@suumit.com>
2017-08-03 21:30:30 +0000

    c7b72aa docs(aio): tech edits to form validation (PR #18495)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-e7e7622
+ * @license Angular v4.3.3-c7b72aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.3.3-e7e7622
+ * @license Angular v4.3.3-c7b72aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
