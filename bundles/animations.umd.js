3591ecfe69fabb15eff014323d5b0d4852f6010b
Victor Berchet <victor@suumit.com>
2017-08-09 21:48:59 +0000

    b14250b test(aio): fix the `deploy-to-firebase` tests

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-2f9d8ff
+ * @license Angular v5.0.0-beta.2-b14250b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.2-2f9d8ff
+ * @license Angular v5.0.0-beta.2-b14250b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
