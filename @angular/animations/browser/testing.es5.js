af08dc022c11bf28632a79bace77b0a32b6e0bb2
Miško Hevery <misko@hevery.com>
2017-08-17 03:14:32 +0000

    0cc77b4 refactor(compiler): split compiler and core (#18683)

--- @angular/animations/browser/testing.es5.js
+++ @angular/animations/browser/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-3a50098
+ * @license Angular v5.0.0-beta.4-0cc77b4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
