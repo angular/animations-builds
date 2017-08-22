89c12072d9b04b74dfad9b6b98fa48815f4f3091
Miško Hevery <misko@hevery.com>
2017-08-22 21:24:43 +0000

    079d884 feat(common): drop use of the Intl API to improve browser support (#18284)

--- @angular/animations/browser.es5.js
+++ @angular/animations/browser.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-409688f
+ * @license Angular v5.0.0-beta.4-079d884
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
