6c9d52060b5ec56aaa41ffbd05a5388e8043de82
Miško Hevery <misko@hevery.com>
2017-08-22 22:27:30 +0000

    d1c4a94 refactor(router): remove deprecated `RouterOutlet` properties (#18781)

--- bundles/animations-browser-testing.umd.js
+++ bundles/animations-browser-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-079d884
+ * @license Angular v5.0.0-beta.4-d1c4a94
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.4-079d884
+ * @license Angular v5.0.0-beta.4-d1c4a94
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
