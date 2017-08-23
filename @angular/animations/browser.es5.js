6223ec986015ca800bbb9f05e83538dd67fa3e03
Marc Laval <mlaval@amadeus.com>
2017-08-23 09:39:10 +0000

    4e03c5a docs: add BrowserStack badge to README.md

--- @angular/animations/browser.es5.js
+++ @angular/animations/browser.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-0914509
+ * @license Angular v5.0.0-beta.4-4e03c5a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- @angular/animations/browser/testing.js
+++ @angular/animations/browser/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-0914509
+ * @license Angular v5.0.0-beta.4-4e03c5a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
