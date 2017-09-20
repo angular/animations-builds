486800bcaf4a57ccdcfcb5c205dbc0deb23e8198
GitHub <noreply@github.com>
2017-09-20 17:25:54 +0000

    3215c4b build: pin docker image to a tag for CircleCI

--- browser/src/dsl/animation_dsl_visitor.d.ts
+++ browser/src/dsl/animation_dsl_visitor.d.ts
@@ -5,14 +5,20 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { AnimationAnimateMetadata, AnimationGroupMetadata, AnimationKeyframesSequenceMetadata, AnimationMetadata, AnimationSequenceMetadata, AnimationStateMetadata, AnimationStyleMetadata, AnimationTransitionMetadata } from '@angular/animations';
+import { AnimationAnimateChildMetadata, AnimationAnimateMetadata, AnimationAnimateRefMetadata, AnimationGroupMetadata, AnimationKeyframesSequenceMetadata, AnimationMetadata, AnimationQueryMetadata, AnimationReferenceMetadata, AnimationSequenceMetadata, AnimationStaggerMetadata, AnimationStateMetadata, AnimationStyleMetadata, AnimationTransitionMetadata, AnimationTriggerMetadata } from '@angular/animations';
 export interface AnimationDslVisitor {
+    visitTrigger(ast: AnimationTriggerMetadata, context: any): any;
     visitState(ast: AnimationStateMetadata, context: any): any;
     visitTransition(ast: AnimationTransitionMetadata, context: any): any;
     visitSequence(ast: AnimationSequenceMetadata, context: any): any;
     visitGroup(ast: AnimationGroupMetadata, context: any): any;
     visitAnimate(ast: AnimationAnimateMetadata, context: any): any;
     visitStyle(ast: AnimationStyleMetadata, context: any): any;
-    visitKeyframeSequence(ast: AnimationKeyframesSequenceMetadata, context: any): any;
+    visitKeyframes(ast: AnimationKeyframesSequenceMetadata, context: any): any;
+    visitReference(ast: AnimationReferenceMetadata, context: any): any;
+    visitAnimateChild(ast: AnimationAnimateChildMetadata, context: any): any;
+    visitAnimateRef(ast: AnimationAnimateRefMetadata, context: any): any;
+    visitQuery(ast: AnimationQueryMetadata, context: any): any;
+    visitStagger(ast: AnimationStaggerMetadata, context: any): any;
 }
 export declare function visitAnimationNode(visitor: AnimationDslVisitor, node: AnimationMetadata, context: any): any;
