11f86bbb48b774d056eee516453497f5fcf8c1c2
Victor Berchet <victor@suumit.com>
2017-09-25 20:02:43 +0000

    4767902 fix(compiler-cli): do not validate metadata from declaration files (#19324)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e224e3d
+ * @license Angular v5.0.0-beta.7-4767902
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.7-e224e3d
+ * @license Angular v5.0.0-beta.7-4767902
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
