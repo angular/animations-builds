3f0608fe888486464cc8a85d1c62eadb263554ad
Victor Berchet <victor@suumit.com>
2017-09-27 21:08:28 +0000

    55a7443 docs(aio): edit summary and next step headers (#16962)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-bc81fbd
+ * @license Angular v4.4.3-55a7443
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v4.4.3-bc81fbd
+ * @license Angular v4.4.3-55a7443
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
