9f26396a6349e88971fb8b3394025803796166c9
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 21:22:34 +0000

    734378c fix(compiler): `TestBed.overrideProvider` should keep imported `NgModule`s eager (#19624)

--- @angular/animations/browser/testing.es5.js
+++ @angular/animations/browser/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.4-3232125
+ * @license Angular v4.4.4-734378c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
