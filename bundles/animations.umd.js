adbd40213ae11e6095d1026edad9069ec45ce884
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 22:15:07 +0000

    703fcda docs: add changelog for 5.0.0-rc.6

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-fde5f2f
+ * @license Angular v5.0.0-rc.6-703fcda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.5-fde5f2f
+ * @license Angular v5.0.0-rc.6-703fcda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
