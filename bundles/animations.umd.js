a6129f6b437786ed3cbd303f8184ce93e4a45e72
Matias Niemelä <matias@yearofmoo.com>
2017-10-28 06:07:53 +0000

    a01c877 docs: ensure examples get rxjs ^5.5.0 (#19985)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.7-5542517
+ * @license Angular v5.0.0-rc.7-a01c877
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.7-5542517
+ * @license Angular v5.0.0-rc.7-a01c877
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
