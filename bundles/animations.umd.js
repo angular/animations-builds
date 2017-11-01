b5b7d833592b5ddeedf820cc2a96416ca167bed9
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:45:23 +0000

    ca129ba docs(aio): upgrade to v5 and cli 1.5

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-171ae15
+ * @license Angular v5.0.0-ca129ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.0-171ae15
+ * @license Angular v5.0.0-ca129ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
