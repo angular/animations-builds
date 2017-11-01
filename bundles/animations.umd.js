2da8b0c9ef552970720a9b572691f63bc5e0fabf
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 23:02:46 +0000

    27ae0f9 build(aio): upgrade to @angular/material@2.0.0-beta.12 (#19702)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-280dada
+ * @license Angular v5.0.0-27ae0f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.0-280dada
+ * @license Angular v5.0.0-27ae0f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
