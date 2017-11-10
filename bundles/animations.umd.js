627277491f55c4f19feb0155f8bef9acadea4750
Jason Aden <jasonaden@google.com>
2017-11-10 22:05:09 +0000

    aa3d75b build: update to google-closure-compiler@20171023.0.1 (#20321)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-2ca6bdd
+ * @license Angular v5.0.1-aa3d75b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.1-2ca6bdd
+ * @license Angular v5.0.1-aa3d75b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
