140b403a96ad78be60ce1c704e5419aeae840c4c
Jason Aden <jasonaden@google.com>
2017-11-15 20:18:06 +0000

    2d6126e style(core): fix comment format for linter

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-bc4b4b5
+ * @license Angular v5.0.1-2d6126e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.1-bc4b4b5
+ * @license Angular v5.0.1-2d6126e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
