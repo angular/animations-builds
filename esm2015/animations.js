8134233475fb60d0c8a7757bfdd50ac5006086b8
Miško Hevery <misko@hevery.com>
2017-11-16 01:00:43 +0000

    f8658cd Revert "fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)"

--- esm2015/animations.js
+++ esm2015/animations.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1269,15 +1269,6 @@ class NoopAnimationPlayer {
      * @return {?}
      */
     getPosition() { return 0; }
-    /**
-     * @param {?} phaseName
-     * @return {?}
-     */
-    triggerCallback(phaseName) {
-        const /** @type {?} */ methods = phaseName == 'start' ? this._onStartFns : this._onDoneFns;
-        methods.forEach(fn => fn());
-        methods.length = 0;
-    }
 }
 
 /**
@@ -1458,15 +1449,6 @@ class AnimationGroupPlayer {
                 player.beforeDestroy();
             }
         });
-    }
-    /**
-     * @param {?} phaseName
-     * @return {?}
-     */
-    triggerCallback(phaseName) {
-        const /** @type {?} */ methods = phaseName == 'start' ? this._onStartFns : this._onDoneFns;
-        methods.forEach(fn => fn());
-        methods.length = 0;
     }
 }
 
