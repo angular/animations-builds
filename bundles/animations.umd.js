2d244facec531cfbdcbd4f99a0073c1eff4851d3
Miško Hevery <misko@hevery.com>
2017-11-29 18:29:57 +0000

    65a2cb8 fix(core): should use native addEventListener in ngZone (#20672)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-c2b3792
+ * @license Angular v5.1.0-beta.2-65a2cb8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.1.0-beta.2-c2b3792
+ * @license Angular v5.1.0-beta.2-65a2cb8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
