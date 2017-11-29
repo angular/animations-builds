310b86f31a49d8b624d67447df0de116d06510c7
Miško Hevery <misko@hevery.com>
2017-11-29 22:33:48 +0000

    ba850b3 Revert "fix(core): should use native addEventListener in ngZone (#20672)"

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-65a2cb8
+ * @license Angular v5.1.0-beta.2-ba850b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.1.0-beta.2-65a2cb8
+ * @license Angular v5.1.0-beta.2-ba850b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
