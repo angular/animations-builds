cba4e937f3823c3541bd830f46dd07cf59af1e4e
Igor Minar <igor@angularjs.org>
2017-12-06 06:07:39 +0000

    a2fde2b ci(core): Improve the payload size message (#20786)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-e37f7c0
+ * @license Angular v5.0.5-a2fde2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.0.5-e37f7c0
+ * @license Angular v5.0.5-a2fde2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
