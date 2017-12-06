20a977fc337e31d1c99c2309941756f7ef2fff7d
Igor Minar <igor@angularjs.org>
2017-12-06 16:44:33 +0000

    b78ada1 fix(animations): ensure DOM is cleaned up after multiple @trigger leave animations finish (#20740)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-be99449
+ * @license Angular v5.1.0-rc.1-b78ada1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.1.0-rc.1-be99449
+ * @license Angular v5.1.0-rc.1-b78ada1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1399,7 +1399,6 @@ var AnimationGroupPlayer = /** @class */ (function () {
         }
         else {
             this.players.forEach(function (player) {
-                player.parentPlayer = _this;
                 player.onDone(function () {
                     if (++doneCount >= total) {
                         _this._onFinish();
--- esm2015/animations.js
+++ esm2015/animations.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-be99449
+ * @license Angular v5.1.0-rc.1-b78ada1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1314,7 +1314,6 @@ class AnimationGroupPlayer {
         }
         else {
             this.players.forEach(player => {
-                player.parentPlayer = this;
                 player.onDone(() => {
                     if (++doneCount >= total) {
                         this._onFinish();
