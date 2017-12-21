81f963392b4f59c5b510824b8e54981d39c88df0
Igor Minar <igor@angularjs.org>
2017-12-21 22:23:42 +0000

    4337768 build: fix pullapprove (#21140)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
