9c2627702f98ae88bda0c785bcf77661e3e41eb9
Igor Minar <igor@angularjs.org>
2017-12-22 17:22:10 +0000

    fa0e8ef fix(common): handle JS floating point errors in percent pipe (#20329)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-0aa6341
+ * @license Angular v5.1.2-fa0e8ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.1.2-0aa6341
+ * @license Angular v5.1.2-fa0e8ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
