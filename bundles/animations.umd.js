d478e7da7212d38827366337cd21d1a3f530a0d0
Kara Erickson <karakara@google.com>
2018-01-04 22:58:11 +0000

    b7307c9 fix(aio): fix test message's mistake. (#21254)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-cc0b0a4
+ * @license Angular v5.1.3-b7307c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.1.3-cc0b0a4
+ * @license Angular v5.1.3-b7307c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
