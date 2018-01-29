e1adf7761bb95f7c5646abfaf64141fe810aadad
Jason Aden <jasonaden@google.com>
2018-01-29 20:00:19 +0000

    6e8bc31 docs: change ”it's" to "its" as needed in several docs. (#21867)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-c40ae7f
+ * @license Angular v6.0.0-beta.1-6e8bc31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.1-c40ae7f
+ * @license Angular v6.0.0-beta.1-6e8bc31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
