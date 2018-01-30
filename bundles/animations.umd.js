2419f1ceb18efc9ab5469b297fa0c1f52080b0d9
Jason Aden <jasonaden@google.com>
2018-01-30 06:16:19 +0000

    02483a0 Revert: "refactor(ivy): save check methods separately"

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-120bdee
+ * @license Angular v6.0.0-beta.1-02483a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.1-120bdee
+ * @license Angular v6.0.0-beta.1-02483a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
