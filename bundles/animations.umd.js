4761edfd20a997f7021661cf2e26496be32f9cf0
Jason Aden <jasonaden@google.com>
2018-01-31 18:39:41 +0000

    81306c1 feat(ivy): add support for content query (#21912)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-06d6c76
+ * @license Angular v6.0.0-beta.1-81306c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.1-06d6c76
+ * @license Angular v6.0.0-beta.1-81306c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
