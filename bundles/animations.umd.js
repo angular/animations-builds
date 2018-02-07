8f2b5ebc5d781b7e4f162a2d347929774564edad
Miško Hevery <misko@hevery.com>
2018-02-07 21:51:44 +0000

    efc67ee fix(ivy): make pipe invocation locality neutral (#22030)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7a406a3
+ * @license Angular v6.0.0-beta.3-efc67ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.3-7a406a3
+ * @license Angular v6.0.0-beta.3-efc67ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
