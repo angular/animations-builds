9fe5310cf59d89732257f3e69cca15bd675128f9
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:51:07 +0000

    22d548f docs(aio): remove ngATL banner from homepage (#22060)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-799edd1
+ * @license Angular v5.2.4-22d548f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.2.4-799edd1
+ * @license Angular v5.2.4-22d548f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
