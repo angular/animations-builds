5411ffbf774aaab7f99298d5a33a209e69baa620
Alex Eagle <alexeagle@google.com>
2018-02-27 02:12:05 +0000

    d033106 docs: update i18n guide for projects that don't use the cli (#21767)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
