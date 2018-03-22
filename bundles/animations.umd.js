b0f96000c001a0713d68675e128992ebfab9be79
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:42:50 +0000

    49f6d1d build: rm --noimplicit_deps from bazel query (#22912)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-641cc49
+ * @license Angular v5.2.9-49f6d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v5.2.9-641cc49
+ * @license Angular v5.2.9-49f6d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
