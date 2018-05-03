d1fac39580c766771469ce7ae1c515a3922d7556
Igor Minar <igor@angular.io>
2018-05-03 23:26:15 +0000

    4ddeb03 build(aio): use Angular 6.0.0 (#23687)

--- bundles/animations.umd.js
+++ bundles/animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-947ea17
+ * @license Angular v6.0.0-4ddeb03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports) { 'use strict';
 
 /**
- * @license Angular v6.0.0-947ea17
+ * @license Angular v6.0.0-4ddeb03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
