8ca1a0cd516d86573e57c75f0623c3e717be625d
Miško Hevery <misko@hevery.com>
2018-07-06 18:27:14 +0000

    1e74ea9 build(bazel): update to rule_nodejs 0.10.0 (#24759)

--- fesm2015/browser/testing.js
+++ fesm2015/browser/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+45.sha-fddd2af
+ * @license Angular v6.1.0-beta.3+47.sha-1e74ea9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -121,11 +121,6 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/**
- * @module
- * @description
- * Entry point for all public APIs of this package.
- */
 
 /**
  * @license
@@ -134,10 +129,6 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-// This file is not used to build this module. It is only used during editing
-// by the TypeScript language service and during build for verifcation. `ngc`
-// replaces this file with production index.ts when it rewrites private symbol
-// names.
 
 export { MockAnimationDriver, MockAnimationPlayer };
 //# sourceMappingURL=browser__testing.js.map
