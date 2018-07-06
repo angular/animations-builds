7e6e895fb39e8046031743dca1669f79f4e44261
Miško Hevery <misko@hevery.com>
2018-07-06 21:46:57 +0000

    3980640 feat(ivy): properly apply style="", [style], [style.foo] and [attr.style] bindings (#24602)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+60.sha-52d43a9
+ * @license Angular v6.1.0-beta.3+61.sha-3980640
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __decorate, __read, __spread, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
