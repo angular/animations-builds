5ade16219a3a9b6f0e347df6d702518f43d66054
Miško Hevery <misko@hevery.com>
2018-07-10 19:37:46 +0000

    49df4ef docs: add tree-shakable providers (#24481)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+71.sha-0d5f2d3
+ * @license Angular v6.1.0-beta.3+73.sha-49df4ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __read, __spread, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
