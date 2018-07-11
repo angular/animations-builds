10c0090d6f57c694d84f48979387f971568d9887
Miško Hevery <misko@hevery.com>
2018-07-11 03:00:54 +0000

    ffb9dc6 docs: fix incorrect forms selector references (#22631)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+85.sha-b7823e7
+ * @license Angular v6.1.0-beta.3+83.sha-ffb9dc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
