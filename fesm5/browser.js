755e6567c17ba0990334703e2963101c59baae5b
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:53:48 +0000

    85d9c20 docs(aio): Add Angular Training to list of training companies (#23907) PR Close #23907

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+107.sha-9a6f27c
+ * @license Angular v6.1.0-beta.3+109.sha-85d9c20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
