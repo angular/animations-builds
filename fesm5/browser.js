afff23eca8775724c8c5e5937e0b80102f98b716
Matias Niemelä <matias@yearofmoo.com>
2018-07-16 20:35:42 +0000

    e97d961 fix(compiler): i18n_extractor now outputs the correct source file name (#24885) for non-inline templates

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+10.sha-d42a662
+ * @license Angular v6.0.9+11.sha-e97d961
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __read, __spread, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
