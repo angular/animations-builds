5f674fe6a0a98d9d89517f8d087d8407d5d07959
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 13:08:33 +0000

    ae04929 feat(ivy): implement initial ngcc package transformer

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-31b4043
+ * @license Angular v6.1.0-beta.3+129.sha-ae04929
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
