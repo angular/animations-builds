c22039245206856868a7e169f26ed4e7c1f6cef6
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:04:56 +0000

    ba3eb8b feat(ivy): properly apply class="", [class], [class.foo] and [attr.class] bindings (#24822)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+117.sha-c8ad965
+ * @license Angular v6.1.0-beta.3+118.sha-ba3eb8b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __decorate, __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
