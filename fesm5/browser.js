38345bb12b882bf4d1a366a6888f28d02535ee73
Matias Niemelä <matias@yearofmoo.com>
2018-07-18 18:37:12 +0000

    23dc9a9 docs: fix typo in user input guide (#22630)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+120.sha-0b28732
+ * @license Angular v6.1.0-beta.3+121.sha-23dc9a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __decorate, __read, __spread, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
