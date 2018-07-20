53a8a02c1070241012c0bd52ac53731d93a9defd
Victor Berchet <victor@suumit.com>
2018-07-20 19:28:49 +0000

    5a1ca35 docs: remove closing parenthesis from provides guide (#24935)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+26.sha-2a5a05b
+ * @license Angular v6.0.9+27.sha-5a1ca35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
