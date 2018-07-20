0dfaca50b4e6065679df74e974bc7d53cc221ff0
Victor Berchet <victor@suumit.com>
2018-07-20 19:57:31 +0000

    4834fda fix(service-worker): don't include sourceMappingURL in ngsw-worker (#24877)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+28.sha-f2aeb70
+ * @license Angular v6.0.9+29.sha-4834fda
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __read, __spread, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
