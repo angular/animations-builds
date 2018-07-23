fe83dd2fffc49cd9f6b2a5fa8a981020131628fb
Victor Berchet <victor@suumit.com>
2018-07-23 19:13:27 +0000

    060867d build: Fix windows tests (#24927)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+38.sha-4d840cb
+ * @license Angular v6.0.9+39.sha-060867d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
