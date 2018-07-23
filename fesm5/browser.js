c102bdf5457c02a3fa65749cc59bb03f3f032dfe
Victor Berchet <victor@suumit.com>
2018-07-23 17:35:32 +0000

    edef58f build(docs-infra): ensure all API headings are sentence cased (#24949)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+50.sha-7c89af3
+ * @license Angular v6.1.0-rc.3+51.sha-edef58f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
