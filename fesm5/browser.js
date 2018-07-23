9bcb37c76418e076d32fbc8d7dd45929638a2bd6
Victor Berchet <victor@suumit.com>
2018-07-23 16:54:35 +0000

    68df0d2 docs: update rxjs link to version 6 (#24269)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+30.sha-d3656bc
+ * @license Angular v6.0.9+31.sha-68df0d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
