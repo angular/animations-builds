c9bbee8ea68140bb83b1e4e38dd749127c4b4e96
Victor Berchet <victor@suumit.com>
2018-07-24 18:41:52 +0000

    6a74ef0 docs: fix instructions for switching directories (#24439)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+33.sha-dc0340c
+ * @license Angular v6.0.9+32.sha-6a74ef0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
