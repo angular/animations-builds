fed7c1370e0aa340deeaf085f12c9da378b1483e
Victor Berchet <victor@suumit.com>
2018-07-25 04:27:28 +0000

    93d325c docs: refactor style guide example 03-06 (#24996)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+44.sha-c7d147e
+ * @license Angular v6.0.9+45.sha-93d325c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
