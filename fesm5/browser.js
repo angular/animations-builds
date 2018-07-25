141cc2cc07c6d7c6cdb6de09cb36c42d2d78a45b
Victor Berchet <victor@suumit.com>
2018-07-25 04:39:59 +0000

    a38127f docs: replace angular/http with HttpClient (#25068)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+47.sha-cd811da
+ * @license Angular v6.0.9+48.sha-a38127f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
