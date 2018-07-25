80dd3ce4ef3d86f7597bdae4d2f8ee7d40d2885e
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-25 07:16:21 +0000

    70668f7 refactor(ivy): do not deep import from ngtsc into ngcc

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+71.sha-0650f32
+ * @license Angular v6.1.0-rc.3+85.sha-70668f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
