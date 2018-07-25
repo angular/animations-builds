2d71717910695f4522ae84444b36eebaafabb9a0
Victor Berchet <victor@suumit.com>
2018-07-25 20:09:07 +0000

    e1c6fd5 Revert "feat(core): add support for using async/await with Jasmine" (#25096)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+74.sha-968f153
+ * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
