ec6e5481a556254971bcfbb3e30d85b7b2968beb
Victor Berchet <victor@suumit.com>
2018-07-25 18:20:14 +0000

    0bcf20c docs(animations): typo fix in the comments (#22652)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+71.sha-cf81823
+ * @license Angular v6.1.0-rc.3+72.sha-0bcf20c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
