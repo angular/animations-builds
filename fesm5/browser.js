b25fbaf1ddeff65b5100a072d969e3b24063683a
Igor Minar <igor@angular.io>
2018-07-27 17:43:13 +0000

    a1b185b docs: Change unnecessary step in ToH-Tutorial (#25059)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+13.sha-601064e
+ * @license Angular v6.1.0+14.sha-a1b185b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
