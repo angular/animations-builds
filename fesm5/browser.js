655c622a2e1e8bb6e1e4b1f42e64c9cd45c76e17
Igor Minar <igor@angular.io>
2018-07-27 19:52:33 +0000

    ce98634 build(compiler-cli): update tsickle dependency to support TypeScript 2.9 (#25152)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+18.sha-3426784
+ * @license Angular v6.1.0+19.sha-ce98634
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
