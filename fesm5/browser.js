576b849d17acc193664e51f1fcdb1067e64dfdeb
Igor Minar <igor@angular.io>
2018-07-27 17:56:48 +0000

    e8d4211 feat(docs-infra): allow notification bar to show arbitrary content (#25020)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+15.sha-a3cf61b
+ * @license Angular v6.1.0+17.sha-e8d4211
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
