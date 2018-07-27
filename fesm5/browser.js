da74445543094a0c9fe86af3860efe1b8049958a
Igor Minar <igor@angular.io>
2018-07-27 00:44:53 +0000

    f2bf828 build(bazel): add comment about angular bazel rules API re-export from /index.bzl (#24663)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0
+ * @license Angular v6.1.0+5.sha-f2bf828
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
