0187e4f5e148ff1c610f96eb134b28e194f1d2ee
Igor Minar <igor@angular.io>
2018-07-28 01:04:56 +0000

    f82b6b2 build(bazel): fix typos in comments (#25172)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+40.sha-a87d44c
+ * @license Angular v6.1.0+41.sha-f82b6b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
