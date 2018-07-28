522f5f26b20dc75609f1ca891e646e418665664e
Igor Minar <igor@angular.io>
2018-07-28 01:04:11 +0000

    6a797d5 refactor(ivy): `element` and `ElementStart` retuns `void` (#25173)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+41.sha-f82b6b2
+ * @license Angular v6.1.0+43.sha-6a797d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
