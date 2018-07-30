dc4b56baeafda43c66bf2106ed91b24e26c6a094
Igor Minar <igor@angular.io>
2018-07-30 22:25:16 +0000

    f5e1802 docs: refactor pipe example to use the HttpClient (#22741)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+11.sha-317c708
+ * @license Angular v6.1.0+12.sha-f5e1802
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
