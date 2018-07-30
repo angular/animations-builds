a26ccffefb42e7cd26b1e3444c8b5f322482c6a5
Igor Minar <igor@angular.io>
2018-07-30 23:54:08 +0000

    22ebd53 docs: update bootstrapping and entry component guide to use httpclient (#25178)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+13.sha-a972c03
+ * @license Angular v6.1.0+14.sha-22ebd53
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
