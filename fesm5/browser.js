88ded91e2c3443eb3a0c5ce8f45f122505023421
Igor Minar <igor@angular.io>
2018-07-30 23:46:10 +0000

    a972c03 docs: fix typo in dependency injection guide (#24972)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+12.sha-f5e1802
+ * @license Angular v6.1.0+13.sha-a972c03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
