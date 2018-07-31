61313a772c3617dbeeb049cdc0e27181df310e87
Igor Minar <igor@angular.io>
2018-07-31 00:41:16 +0000

    e99d860 feat(compiler): add "original" placeholder value on extracted XMB (#25079)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+51.sha-24789e9
+ * @license Angular v6.1.0+52.sha-e99d860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
