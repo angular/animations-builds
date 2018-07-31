5c1146d7cab7ca32fca75dc5f8e7ecadb743c4aa
Igor Minar <igor@angular.io>
2018-07-31 05:35:06 +0000

    e3834b7 feat(ivy): support change detection on the root view (#25085)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+56.sha-3f20a2f
+ * @license Angular v6.1.0+55.sha-e3834b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
