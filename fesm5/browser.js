b74a1b366a714041d983a4d6662627ef8cf1df15
Igor Minar <igor@angular.io>
2018-07-31 05:29:38 +0000

    3f20a2f docs: fix link to "Override component providers" (#24967)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+54.sha-3664829
+ * @license Angular v6.1.0+56.sha-3f20a2f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
