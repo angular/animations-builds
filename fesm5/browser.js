0f7309f24f85317690e0a1ea43246b867acc1f65
Igor Minar <igor@angular.io>
2018-07-31 20:59:30 +0000

    fa59748 build: make `postinstall` script compatible with Windows (#25232)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+21.sha-fa38828
+ * @license Angular v6.1.0+20.sha-fa59748
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __extends, __spread, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
