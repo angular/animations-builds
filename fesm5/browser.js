af40ffcbbf09eec0da1c3e0d4a4e30b2b0da6b0a
Igor Minar <igor@angular.io>
2018-07-31 00:37:25 +0000

    24789e9 docs(aio): add StrongBrew to the trainer list (#24891)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+50.sha-f94f964
+ * @license Angular v6.1.0+51.sha-24789e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
