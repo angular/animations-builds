72456cf49e856ad2bedca2869649785d216c5e82
Igor Minar <igor@angular.io>
2018-08-01 20:10:23 +0000

    2651604 refactor(animations): do not use short parameter names (#25198)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+24.sha-3ab31a4
+ * @license Angular v6.1.0+27.sha-2651604
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
