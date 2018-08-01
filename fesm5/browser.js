54721064ab953ff7b8d6c6f2a17714ed901fa502
Igor Minar <igor@angular.io>
2018-08-01 20:08:26 +0000

    43dcf77 build(bazel): fix typo in protractor test target definition (#25235)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+26.sha-a83b9f7
+ * @license Angular v6.1.0+23.sha-43dcf77
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
