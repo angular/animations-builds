d58b23f28137936bbf376d2e5ba1ed826de66cb8
Igor Minar <igor@angular.io>
2018-08-01 22:29:54 +0000

    06af794 test(upgrade): run tests against AngularJS v1.7.x as well (#25231)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+83.sha-bde4402
+ * @license Angular v6.1.0+85.sha-06af794
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
