085f029b73b3442538aaf85d3c42601fd030af0b
Igor Minar <igor@angular.io>
2018-08-01 20:18:17 +0000

    1e53278 docs(core): replace ReflectiveInjector example with Static Injector example (#25162)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+67.sha-4976a58
+ * @license Angular v6.1.0+72.sha-1e53278
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -948,7 +948,7 @@ class NoopAnimationPlayer {
         }
     }
     reset() { }
-    setPosition(p) { }
+    setPosition(position) { }
     getPosition() { return 0; }
     /** @internal */
     triggerCallback(phaseName) {
