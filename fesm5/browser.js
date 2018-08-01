1c92a3236943ac6ca05c172874fca36584ce5463
Igor Minar <igor@angular.io>
2018-08-01 20:29:09 +0000

    7a4fb44 docs(aio): add Kevin Yang to GDE resources (#24791)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+74.sha-a9ecf4b
+ * @license Angular v6.1.0+77.sha-7a4fb44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
