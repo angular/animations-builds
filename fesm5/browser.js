26bc6edd2e95d67b106c098b4a5d26270c1001e8
Igor Minar <igor@angular.io>
2018-08-01 20:14:27 +0000

    0452712 docs: refactor lazy loading modules example (#25071)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+30.sha-ec31f6b
+ * @license Angular v6.1.0+31.sha-0452712
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
