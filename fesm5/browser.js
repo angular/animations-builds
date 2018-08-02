264bf8df0a695b3907cd282fc18a7d1ccfe0e2ab
Igor Minar <igor@angular.io>
2018-08-02 00:18:13 +0000

    d5d034a docs: update reactiveconf 2018 in events (#24739)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+43.sha-5ca35b3
+ * @license Angular v6.1.0+44.sha-d5d034a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
