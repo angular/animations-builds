e2aa5588759893ac8a3d9c82f3a2b8a5df6599a2
Igor Minar <igor@angular.io>
2018-08-02 17:38:51 +0000

    74bce18 Revert "docs: refactor http module import for style guide app.module (#25001)" (#25263)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+97.sha-3ba5220
+ * @license Angular v6.1.0+98.sha-74bce18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
