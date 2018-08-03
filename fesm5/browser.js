17045384c8690dd4afed958ca9cec2ebdb54e414
Kara Erickson <karakara@google.com>
2018-08-03 02:59:12 +0000

    c2c12e5 feat(ivy): support ng-container as a child of an already inserted view (#25227)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+1.sha-4f741e7
+ * @license Angular v7.0.0-beta.0+3.sha-c2c12e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
