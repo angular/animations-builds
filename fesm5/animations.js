5c5765beb719206128140eaafc6c6398a7d28797
Igor Minar <igor@angular.io>
2018-08-03 00:33:46 +0000

    183757d fix(core): fix tests for uninitialized @Output error (#19116)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+8.sha-3169edd
+ * @license Angular v6.1.0+100.sha-183757d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -967,7 +967,7 @@ var NoopAnimationPlayer = /** @class */ (function () {
         }
     };
     NoopAnimationPlayer.prototype.reset = function () { };
-    NoopAnimationPlayer.prototype.setPosition = function (p) { };
+    NoopAnimationPlayer.prototype.setPosition = function (position) { };
     NoopAnimationPlayer.prototype.getPosition = function () { return 0; };
     /** @internal */
     NoopAnimationPlayer.prototype.triggerCallback = function (phaseName) {
