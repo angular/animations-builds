b1b858ace0fad3f80be0446017b78b5be7a454b2
Kara Erickson <karakara@google.com>
2018-08-03 23:36:51 +0000

    0c9c2ac refactor(bazel): dont rely on language target to downlevel for loop (#24534)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+3.sha-0fb41e5
+ * @license Angular v6.1.1+4.sha-0c9c2ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
