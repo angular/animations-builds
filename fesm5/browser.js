caecc7d6b52af1c12bbade2d77d2c9ce468f6166
Kara Erickson <karakara@google.com>
2018-08-06 23:16:52 +0000

    3355502 fix(ivy): support ng-container at the root of a view with delayed insertion (#25329)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+38.sha-16c03c0
+ * @license Angular v7.0.0-beta.0+37.sha-3355502
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
