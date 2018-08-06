d62560dccb9586b361f0e5dd3044d7b726b3dd04
Kara Erickson <karakara@google.com>
2018-08-06 22:38:55 +0000

    8d0f8bd docs: fix table in comparing observables guide (#22485)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+19.sha-8cbb836
+ * @license Angular v6.1.1+18.sha-8d0f8bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
