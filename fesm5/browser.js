78723d7511504936c45bad7f1b5400bd2be8b15a
Kara Erickson <karakara@google.com>
2018-08-06 22:29:24 +0000

    c158702 docs: add missing word in outputs description. (#25330)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+27.sha-2b906f6
+ * @license Angular v7.0.0-beta.0+28.sha-c158702
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
