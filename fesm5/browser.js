0bdfbbd24b27bdb9cce7083a8606b9b449e665e3
Kara Erickson <karakara@google.com>
2018-08-06 22:30:54 +0000

    7d006c5 docs(core): fix tree-shakable spelling (#24057)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+28.sha-c158702
+ * @license Angular v7.0.0-beta.0+32.sha-7d006c5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
