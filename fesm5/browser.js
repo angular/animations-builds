69e29efb0c7cd4228ba89ee763c6b21784e22bb1
Kara Erickson <karakara@google.com>
2018-08-06 22:52:13 +0000

    415519a docs: update to 2nd edition of Learning Angular (#20934)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+18.sha-8d0f8bd
+ * @license Angular v6.1.1+20.sha-415519a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
