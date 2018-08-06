23b138ba7bc69feb1b43013b3e58217b918941e1
Kara Erickson <karakara@google.com>
2018-08-06 22:26:53 +0000

    0c028a0 docs: remove code in universal hero detail component (#25215)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+12.sha-40904ce
+ * @license Angular v6.1.1+14.sha-0c028a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
