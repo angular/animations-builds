28c8e4c33ef62a5b449b4e9eda3b6c6ff177f310
Kara Erickson <karakara@google.com>
2018-08-07 18:59:40 +0000

    94a2ac7 docs: update resources to include UI-jar (#21200)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+25.sha-155d938
+ * @license Angular v6.1.1+24.sha-94a2ac7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
