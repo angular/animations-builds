a40c952b2745656ea21a34ca2a262291adfdf55b
Kara <kara@users.noreply.github.com>
2018-08-08 22:12:50 +0000

    2951e72 build(bazel): update to rules_nodejs 0.11.2 and latest rules_typescript (#25365)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+5.sha-6480d1b
+ * @license Angular v6.1.2+7.sha-2951e72
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
