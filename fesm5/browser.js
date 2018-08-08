9c66c67ac4d3a2c36d24b197cc44b3f357b37236
Kara Erickson <karakara@google.com>
2018-08-08 22:02:37 +0000

    6480d1b docs: make css multiline in styleguide for consistency (#25300)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+1.sha-f39551c
+ * @license Angular v6.1.2+5.sha-6480d1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
