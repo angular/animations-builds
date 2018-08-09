53208b29d667c33c70339fa458f30891394858b0
Ben Lesh <ben@benlesh.com>
2018-08-09 15:37:28 +0000

    9c92a6f test(ivy): add test for <ng-container> and shallow queries (#25379)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+9.sha-8bbce3f
+ * @license Angular v7.0.0-beta.1+12.sha-9c92a6f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
