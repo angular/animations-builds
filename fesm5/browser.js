8b160589c7719471aac79e826ec51228d4b0c6f3
Ben Lesh <ben@benlesh.com>
2018-08-09 03:01:48 +0000

    fda30cb build: stop printing source-map-support warning (#25339)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+6.sha-3449f1e
+ * @license Angular v6.1.2+8.sha-fda30cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
