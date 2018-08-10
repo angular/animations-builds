32307a81e1aef7d54207d6830932cdda741b6f42
Ben Lesh <ben@benlesh.com>
2018-08-10 17:30:29 +0000

    209cc7e docs: fix typo in testing guide  (closes #25400) (#25418)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+23.sha-c8c1aa7
+ * @license Angular v7.0.0-beta.1+20.sha-209cc7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
