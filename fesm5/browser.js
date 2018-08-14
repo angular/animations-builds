ce6ed4f0f6aced09a413895821a22741d998fa35
Ben Lesh <ben@benlesh.com>
2018-08-14 20:13:17 +0000

    e906bf4 docs: add Accelebrate to resources (#23204)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+12.sha-f1ed022
+ * @license Angular v6.1.2+14.sha-e906bf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
