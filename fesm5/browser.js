910b7b5f9364f5323d954d926266dfa621043114
Ben Lesh <ben@benlesh.com>
2018-08-14 20:09:06 +0000

    9117fa1 test(ivy): activate local references canonical spec (#25462)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+30.sha-c869b14
+ * @license Angular v7.0.0-beta.1+38.sha-9117fa1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __spread, __read, __extends, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
