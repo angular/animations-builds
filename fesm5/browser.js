10b29b793506f1e9337f9f242cb84fb2b54d5de3
Ben Lesh <ben@benlesh.com>
2018-08-15 01:23:07 +0000

    01ec5fd fix(service-worker): `Cache-Control: no-cache` on assets breaks service worker (#25408)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+49.sha-eeb81b9
+ * @license Angular v7.0.0-beta.1+51.sha-01ec5fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
