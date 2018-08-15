77b8b372561d79bbf2cf40f9ff26a768f8beeb69
Ben Lesh <ben@benlesh.com>
2018-08-15 01:10:13 +0000

    eeb81b9 docs: enable debug tools with current versions of Angular (#25361)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+50.sha-be2cf4d
+ * @license Angular v7.0.0-beta.1+49.sha-eeb81b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
