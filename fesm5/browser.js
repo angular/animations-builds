8b34f14dd521adcca8b75ef6e1c842b9cdd9e3ec
Jason Aden <jasonaden@google.com>
2018-08-16 22:11:58 +0000

    abcc430 build(aio): update dgeni-packages to 0.26.3 to fix reference types issue (#25491)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.2+16.sha-de03abb
+ * @license Angular v7.0.0-beta.2+14.sha-abcc430
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
