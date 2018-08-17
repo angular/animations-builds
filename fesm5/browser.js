8cbc26de11f4243ceeeff7a1f17ba6d7b01560c3
Jason Aden <jasonaden@google.com>
2018-08-17 21:55:14 +0000

    2b810a4 docs(docs-infra): the `build.sh` script was renamed to `create-image.sh` (#25554)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.3+14.sha-a5cc9db
+ * @license Angular v6.1.3+13.sha-2b810a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
