0989319f23afe7073d971857e18478978afb913f
Jason Aden <jasonaden@google.com>
2018-08-21 19:34:25 +0000

    1fa9790 docs: Improve docs for downgrading a service (#19371)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.3+21.sha-7e61645
+ * @license Angular v6.1.3+22.sha-1fa9790
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
