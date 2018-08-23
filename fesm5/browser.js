aa11bfb12238911024c3c1c33bcf839994bab299
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:04:28 +0000

    27e2039 fix(compiler): update compiler to generate new slot allocations (#25607)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+27.sha-22d58fc
+ * @license Angular v7.0.0-beta.3+26.sha-27e2039
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
