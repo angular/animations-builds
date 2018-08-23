11c6dd6a96ec5be711576cdf3d88b31f9e1afe75
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:52:26 +0000

    668bfce fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+28.sha-eb1fe19
+ * @license Angular v7.0.0-beta.3+29.sha-668bfce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
