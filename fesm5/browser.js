0f9113dd689ad913cf4c44c968088d239497dd97
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 21:20:00 +0000

    3634575 test(common): fix double instantiation in NgSwitch test (#25632)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+39.sha-9bcd8c2
+ * @license Angular v7.0.0-beta.3+57.sha-3634575
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
