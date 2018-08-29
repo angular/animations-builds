812e2d7e0949f2ca2d085153d3e6c659e04e988f
Matias Niemelä <matias@yearofmoo.com>
2018-08-29 03:16:53 +0000

    9ee29ec docs: clarification of hero selection in routing section (#25634)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+70.sha-0386c44
+ * @license Angular v7.0.0-beta.3+74.sha-9ee29ec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
