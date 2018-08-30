319bc759d0fa509529e14c5258341d3252daa712
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 00:18:51 +0000

    0a3dd87 build: fix bad merge onto patch branch (#25729)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.4+8.sha-e40519c
+ * @license Angular v6.1.5+2.sha-0a3dd87
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
