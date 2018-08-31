ed32927f202b9268b4552aa0531ff4c727b423cc
Misko Hevery <misko@hevery.com>
2018-08-31 21:40:13 +0000

    6def18a fix(ivy): support directive outputs on ng-template (#25717)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+32.sha-1e3460b
+ * @license Angular v7.0.0-beta.4+34.sha-6def18a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
