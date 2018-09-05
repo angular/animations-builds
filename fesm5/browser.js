d78e7b840b9c80e7f802c4054a556e56792e297b
Misko Hevery <misko@hevery.com>
2018-09-05 20:36:55 +0000

    e72f741 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+22.sha-82e0676
+ * @license Angular v6.1.6+24.sha-e72f741
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
