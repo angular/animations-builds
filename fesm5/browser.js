dab620fba3019ff949143123d18e849f9c2d5b7d
Misko Hevery <misko@hevery.com>
2018-09-05 19:25:04 +0000

    92298e5 build(docs-infra): do not render internals in package API pages (#25723)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+19.sha-23a96dc
+ * @license Angular v6.1.6+17.sha-92298e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
