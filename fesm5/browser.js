8263f32436165c61b7d7fd8a75bf4118a173a5d8
Misko Hevery <misko@hevery.com>
2018-09-05 19:28:50 +0000

    1e7a873 build(bazel): remove unused angular.tsconfig.json integration/bazel test (#25778)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+50.sha-13b8399
+ * @license Angular v7.0.0-beta.4+51.sha-1e7a873
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
