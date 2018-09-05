75195b1a31a6f595086fe301fff5df940a0a586d
Misko Hevery <misko@hevery.com>
2018-09-05 19:28:46 +0000

    27f0817 docs(changelog): fix version 6.1.5 typo (#25760) PR Close #25760

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+18.sha-6f7df8a
+ * @license Angular v6.1.6+16.sha-27f0817
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
