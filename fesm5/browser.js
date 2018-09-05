8a73c9613d69115efb4fb7d26b01eb00f64f28d2
Misko Hevery <misko@hevery.com>
2018-09-05 20:26:14 +0000

    3a0b735 build: upgrade Chromium and ChromeDriver to latest versions (#25602) (#25669)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+23.sha-f0bcfd0
+ * @license Angular v6.1.6+27.sha-3a0b735
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
