093b3220e30c4251bd5bea5f1f80899c92305d80
Igor Minar <igor@angular.io>
2018-09-12 00:43:47 +0000

    8d7fbb6 docs(router): change description of parameter usage for navigation functions (#21840)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+11.sha-55a1ce7
+ * @license Angular v6.1.7+13.sha-8d7fbb6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
