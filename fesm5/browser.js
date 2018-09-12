03d45a220f253659ced26f7c4ca4a18f682535da
Igor Minar <igor@angular.io>
2018-09-12 00:24:41 +0000

    21009b0 fix(ivy): use proper sanitizer names (#25817)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.5+27.sha-00d3666
+ * @license Angular v7.0.0-beta.5+26.sha-21009b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __spread, __read, __assign, __values } from 'tslib';
+import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
