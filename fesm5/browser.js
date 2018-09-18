c6f6707df28dd0fef370294eca4b408b8026f6d7
Ben Lesh <ben@benlesh.com>
2018-09-18 21:38:09 +0000

    274dc1e build(bazel): update to rules_typescript 0.17.0 & rules_nodejs 0.13.4 (#25977)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+18.sha-6c8791e
+ * @license Angular v6.1.7+17.sha-274dc1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
