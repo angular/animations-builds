8e0cea841c74875f6e4ebd5d1c40422260ca30a2
Kara Erickson <karakara@google.com>
2018-09-21 19:00:48 +0000

    ee9f0b5 build(docs-infra): do not include license comment in first API doc (#26050)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8+18.sha-05d1b84
+ * @license Angular v6.1.8+14.sha-ee9f0b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __read, __extends, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
