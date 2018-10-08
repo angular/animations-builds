ed27757663ec0a636cdd91e081d0e3efbd35fd70
Jason Aden <jasonaden@google.com>
2018-10-08 19:39:46 +0000

    623adbb fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+71.sha-3660ff8
+ * @license Angular v6.1.9+72.sha-623adbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
