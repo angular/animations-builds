a4f7522efdcfc23c777d359243f3040acb1f2489
Miško Hevery <misko@hevery.com>
2018-10-11 21:48:46 +0000

    f1e14a3 docs: add angularmix to events page (#26374)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+2.sha-65555fe
+ * @license Angular v6.1.10+4.sha-f1e14a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
