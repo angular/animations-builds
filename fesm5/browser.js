ec2b10322fb1253d762cead1647ad03d0a0d03ce
Miško Hevery <misko@hevery.com>
2018-10-12 04:32:29 +0000

    735dfd3 refactor(ivy): replace LNode.dynamicLContainerNode with flat LContainers (#26407)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+27.sha-071934e
+ * @license Angular v7.0.0-rc.1+26.sha-735dfd3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __extends, __spread, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
