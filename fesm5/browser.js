52e2c5f3a0e75f25dd2fa9d6a28ea370bea33ec9
Miško Hevery <misko@hevery.com>
2018-10-12 16:37:56 +0000

    db49bea build(docs-infra): upgrade @angular/material to 7.0.0-rc.1 (#26394)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+5.sha-4f09f7d
+ * @license Angular v6.1.10+8.sha-db49bea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __spread, __extends, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
