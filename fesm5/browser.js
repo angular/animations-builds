3bb64cc46c2857be82c1f500f232423278ba761b
Misko Hevery <misko@hevery.com>
2018-10-17 19:11:51 +0000

    516515d build(docs-infra): do not add extra space after links in tables (#26505)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+67.sha-948f507
+ * @license Angular v7.0.0-rc.1+68.sha-516515d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
+import { __decorate, __extends, __spread, __read, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
