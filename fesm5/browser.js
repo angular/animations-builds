a3c127fd1a5c7dae65acc1e1310227d21a16cdd1
Miško Hevery <misko@hevery.com>
2018-10-17 20:41:55 +0000

    665627e fix(aio): add relative to app level routes section (#26504)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+86.sha-a609bf5
+ * @license Angular v7.0.0-rc.1+87.sha-665627e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __decorate, __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
