ca37e0b9e6a491087b1f170fe189c2fc7b36f378
Igor Minar <igor@angular.io>
2018-11-30 22:06:40 +0000

    eb17502 fix(bazel): ng_package should correctly map to source maps in secondary entry-points (#27313)

--- fesm2015/browser/testing.js
+++ fesm2015/browser/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+81.sha-c8a8dcf
+ * @license Angular v7.1.0+82.sha-eb17502
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -187,4 +187,4 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
  */
 
 export { MockAnimationDriver, MockAnimationPlayer };
-//# sourceMappingURL=browser__testing.js.map
+//# sourceMappingURL=testing.js.map
