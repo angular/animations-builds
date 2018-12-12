8f0ee47a7f729de2bb16f7a0b59e690f756fcd49
Miško Hevery <misko@hevery.com>
2018-12-12 22:11:49 +0000

    7fabe44 fix(ivy): add support for optional nullable injection tokens (#27552)

--- fesm2015/browser/testing.js
+++ fesm2015/browser/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+26.sha-3290fc3
+ * @license Angular v7.2.0-beta.2+10.sha-7fabe44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { ɵallowPreviousPlayerStylesMerge, ɵcontainsElement, ɵinvokeQuery, ɵm
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /**
  * \@publicApi
@@ -64,7 +64,7 @@ class MockAnimationDriver {
     animate(element, keyframes, duration, delay, easing, previousPlayers = []) {
         /** @type {?} */
         const player = new MockAnimationPlayer(element, keyframes, duration, delay, easing, previousPlayers);
-        MockAnimationDriver.log.push(/** @type {?} */ (player));
+        MockAnimationDriver.log.push((/** @type {?} */ (player)));
         return player;
     }
 }
@@ -104,11 +104,13 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
             });
         }
     }
+    /* @internal */
     /**
      * @param {?} fn
      * @return {?}
      */
     onInit(fn) { this._onInitFns.push(fn); }
+    /* @internal */
     /**
      * @return {?}
      */
@@ -131,6 +133,7 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
         super.destroy();
         this.__finished = true;
     }
+    /* @internal */
     /**
      * @return {?}
      */
@@ -173,17 +176,17 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 
 /**
