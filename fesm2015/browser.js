3ddb6ea474996d916f2d0f4b427f806e8b33c8c5
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:47:49 +0000

    091a8a6 fix(ivy): add missing @nocollapse annotation to __NG_ELEMENT_ID__ fields (#28050)

--- fesm2015/browser.js
+++ fesm2015/browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+122.sha-8c3f98f
+ * @license Angular v7.2.0+128.sha-091a8a6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -363,7 +363,7 @@ class NoopAnimationDriver {
 NoopAnimationDriver.decorators = [
     { type: Injectable },
 ];
-NoopAnimationDriver.ngInjectableDef = defineInjectable({ token: NoopAnimationDriver, factory: function NoopAnimationDriver_Factory(t) { return new (t || NoopAnimationDriver)(); }, providedIn: null });
+/** @nocollapse */ NoopAnimationDriver.ngInjectableDef = defineInjectable({ token: NoopAnimationDriver, factory: function NoopAnimationDriver_Factory(t) { return new (t || NoopAnimationDriver)(); }, providedIn: null });
 /*@__PURE__*/ ɵsetClassMetadata(NoopAnimationDriver, [{
         type: Injectable
     }], null, null);
