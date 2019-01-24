78c4e1b01fb2de193f2bb8114a78e860b6f0348e
Jason Aden <jasonaden@google.com>
2019-01-24 18:49:31 +0000

    465abab docs(animations): fix group and sequence function usage examples (#28305)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+21.sha-c1fb9c2
+ * @license Angular v8.0.0-beta.1+22.sha-465abab
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -304,8 +304,8 @@ function animate(timings, styles) {
  *
  * ```typescript
  * group([
- *   animate("1s", { background: "black" }))
- *   animate("2s", { color: "white" }))
+ *   animate("1s", style({ background: "black" })),
+ *   animate("2s", style({ color: "white" }))
  * ])
  * ```
  *
@@ -340,7 +340,7 @@ function group(steps, options) {
  * ```typescript
  * sequence([
  *   style({ opacity: 0 })),
- *   animate("1s", { opacity: 1 }))
+ *   animate("1s", style({ opacity: 1 }))
  * ])
  * ```
  *
