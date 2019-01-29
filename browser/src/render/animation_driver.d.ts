1023b29392fbf70276e0e1b0baa42d0e42be24ad
Jason Aden <jasonaden@google.com>
2019-01-29 23:33:42 +0000

    dabcb3e docs: fix Redundant "See also:" link in ContentChild (#28334)

--- browser/src/render/animation_driver.d.ts
+++ browser/src/render/animation_driver.d.ts
@@ -1,4 +1,3 @@
-import * as i0 from '@angular/core';
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -7,6 +6,7 @@ import * as i0 from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 import { AnimationPlayer } from '@angular/animations';
+import * as i0 from "@angular/core";
 /**
  * @publicApi
  */
