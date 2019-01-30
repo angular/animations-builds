6dedf9ecdffc17343fc48f7054afb6414995cf86
Jason Aden <jasonaden@google.com>
2019-01-30 01:03:18 +0000

    0ff48a1 style: change to American English (#27266)

--- bundles/animations-browser.umd.js
+++ bundles/animations-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+43.sha-ea2a3f8
+ * @license Angular v7.2.2+44.sha-0ff48a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -697,7 +697,7 @@
      *
      * 1. Overlap of animations
      * Given that a CSS property cannot be animated in more than one place at the same time, it's
-     * important that this behaviour is detected and validated. The way in which this occurs is that
+     * important that this behavior is detected and validated. The way in which this occurs is that
      * each time a style property is examined, a string-map containing the property will be updated with
      * the start and end times for when the property is used within an animation step.
      *
