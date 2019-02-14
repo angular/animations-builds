2f058d2bfe30409ac0fc3ef6cc94d48718be9b9e
Misko Hevery <misko@hevery.com>
2019-02-14 03:46:50 +0000

    36df905 docs(animations): fixed some grammar (#28708) PR Close #28708

--- src/animation_metadata.d.ts
+++ src/animation_metadata.d.ts
@@ -1070,7 +1070,7 @@ export declare function animation(steps: AnimationMetadata | AnimationMetadata[]
  * for a child animation to run, the parent animation must query each of the elements
  * containing child animations, and run them using this function.
  *
- * Note that this feature designed to be used with `query()` and it will only work
+ * Note that this feature is designed to be used with `query()` and it will only work
  * with animations that are assigned using the Angular animation library. CSS keyframes
  * and transitions are not handled by this API.
  *
