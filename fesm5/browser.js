a0c2d67b14a1f6a0c70c765979199a6ac048a489
Kara Erickson <karakara@google.com>
2019-03-06 23:48:41 +0000

    79fb9d4 build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+20.sha-36cbfb1
+ * @license Angular v6.1.10+26.sha-79fb9d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read, __assign, __values } from 'tslib';
+import { __extends, __read, __spread, __assign, __values } from 'tslib';
 import { AUTO_STYLE, NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, sequence, style } from '@angular/animations';
 import { defineInjectable } from '@angular/core';
 
