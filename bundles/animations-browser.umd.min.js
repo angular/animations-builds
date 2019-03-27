8de17b6128d1eb9fe2661d8a88dd25913cd7d3e3
Miško Hevery <misko@hevery.com>
2019-03-27 17:16:19 +0000

    50fb629 docs: add Bonnie to GDE page (#29432)

--- bundles/animations-browser.umd.min.js
+++ bundles/animations-browser.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+6.sha-3514543.with-local-changes
+ * @license Angular v8.0.0-beta.10+10.sha-50fb629.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
