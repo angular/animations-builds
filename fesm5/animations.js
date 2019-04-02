d8374e0b59a9fc1149b056bbab3611be76c9f552
Jason Aden <jasonaden@google.com>
2019-04-02 18:02:34 +0000

    9f54d76 refactor(upgrade): use Bazel packages to avoid symlinks in the source (#29466)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+103.sha-3487055.with-local-changes
+ * @license Angular v8.0.0-beta.10+105.sha-9f54d76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
