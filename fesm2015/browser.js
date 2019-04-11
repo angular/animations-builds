a97c2f9f81e88f89510245f1f46646f41b421119
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-11 23:42:21 +0000

    b057806 refactor(ivy): use ɵɵ instead of Δ for now (#29850)

--- fesm2015/browser.js
+++ fesm2015/browser.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v8.0.0-beta.11+84.sha-54058ba.with-local-changes
+ * @license Angular v8.0.0-beta.11+85.sha-b057806.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
 
 import { NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, AUTO_STYLE, sequence, style } from '@angular/animations';
-import { Injectable, ΔdefineInjectable, ɵsetClassMetadata } from '@angular/core';
+import { Injectable, ɵɵdefineInjectable, ɵsetClassMetadata } from '@angular/core';
 
 /**
  * @fileoverview added by tsickle
@@ -414,7 +414,7 @@ class NoopAnimationDriver {
 NoopAnimationDriver.decorators = [
     { type: Injectable },
 ];
-/** @nocollapse */ NoopAnimationDriver.ngInjectableDef = ΔdefineInjectable({ token: NoopAnimationDriver, factory: function NoopAnimationDriver_Factory(t) { return new (t || NoopAnimationDriver)(); }, providedIn: null });
+/** @nocollapse */ NoopAnimationDriver.ngInjectableDef = ɵɵdefineInjectable({ token: NoopAnimationDriver, factory: function NoopAnimationDriver_Factory(t) { return new (t || NoopAnimationDriver)(); }, providedIn: null });
 /*@__PURE__*/ ɵsetClassMetadata(NoopAnimationDriver, [{
         type: Injectable
     }], null, null);
--- fesm5/browser.js
+++ fesm5/browser.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v8.0.0-beta.11+84.sha-54058ba.with-local-changes
+ * @license Angular v8.0.0-beta.11+85.sha-b057806.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
 
 import { NoopAnimationPlayer, ɵAnimationGroupPlayer, ɵPRE_STYLE, AUTO_STYLE, sequence, style } from '@angular/animations';
-import { ΔdefineInjectable, ɵsetClassMetadata, Injectable } from '@angular/core';
+import { ɵɵdefineInjectable, ɵsetClassMetadata, Injectable } from '@angular/core';
 import { __spread, __read, __extends, __assign, __values } from 'tslib';
 
 function isBrowser() {
@@ -223,7 +223,7 @@ var NoopAnimationDriver = /** @class */ (function () {
         if (previousPlayers === void 0) { previousPlayers = []; }
         return new NoopAnimationPlayer(duration, delay);
     };
-    NoopAnimationDriver.ngInjectableDef = ΔdefineInjectable({ token: NoopAnimationDriver, factory: function NoopAnimationDriver_Factory(t) { return new (t || NoopAnimationDriver)(); }, providedIn: null });
+    NoopAnimationDriver.ngInjectableDef = ɵɵdefineInjectable({ token: NoopAnimationDriver, factory: function NoopAnimationDriver_Factory(t) { return new (t || NoopAnimationDriver)(); }, providedIn: null });
     return NoopAnimationDriver;
 }());
 /*@__PURE__*/ ɵsetClassMetadata(NoopAnimationDriver, [{
