7c714a6280a372f27c3658ae90ede927dd344884
Miško Hevery <misko@hevery.com>
2019-07-26 03:28:05 +0000

    6ece7db37 build: TypeScript 3.5 upgrade (#31615)

--- esm2015/browser/src/render/css_keyframes/css_keyframes_player.js
+++ esm2015/browser/src/render/css_keyframes/css_keyframes_player.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 import { computeStyle } from '../../util';
 import { ElementAnimationStyleHandler } from './element_animation_style_handler';
