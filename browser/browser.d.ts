5d702c92be555f56b98ed0033e8c86bcf6cf88e0
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:23:45 +0000

    0757702d6 docs: add guide to reproduce material-unit-test failures locally (#32138)

--- browser/browser.d.ts
+++ browser/browser.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+12.sha-baf2d9d.with-local-changes
+ * @license Angular v8.2.2+14.sha-0757702.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
