558e1c253801166a9082cc0bcd8c759624ee750b
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 23:29:13 +0000

    9166baf70 refactor(core): Migrate TestBed.get to TestBed.inject (#32382)

--- fesm2015/browser.js
+++ fesm2015/browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+46.sha-a391aeb.with-local-changes
+ * @license Angular v9.0.0-next.5+48.sha-9166baf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6156,7 +6156,7 @@ class TransitionAnimationEngine {
              */
             player => {
                 /** @type {?} */
-                const realPlayer = (/** @type {?} */ (player.getRealPlayer()));
+                const realPlayer = (/** @type {?} */ (((/** @type {?} */ (player))).getRealPlayer()));
                 if (realPlayer.beforeDestroy) {
                     realPlayer.beforeDestroy();
                 }
