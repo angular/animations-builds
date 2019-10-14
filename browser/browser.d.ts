5e19e35edcbf478afe0d6bc5dd1fc1eb5c09b7ad
Miško Hevery <misko@hevery.com>
2019-10-14 23:27:33 +0000

    fc93dafab refactor(core): rename ngModuleDef to ɵmod (#33142)

--- browser/browser.d.ts
+++ browser/browser.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+70.sha-c3aaa52.with-local-changes
+ * @license Angular v9.0.0-next.10+75.sha-fc93daf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
