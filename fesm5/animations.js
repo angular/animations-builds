76a4380ed7b850092e805537d30f883480dd84bb
atscott <atscott01@gmail.com>
2019-11-05 22:31:19 +0000

    d751ca7596 fix(core): renderer-to-renderer2 migration not migrating methods (#33571)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+47.sha-85298e3.with-local-changes
+ * @license Angular v9.0.0-rc.0+62.sha-d751ca7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1183,5 +1183,5 @@ var ɵPRE_STYLE = '!';
  * found in the LICENSE file at https://angular.io/license
  */
 
-export { AnimationBuilder, AnimationFactory, AUTO_STYLE, animate, animateChild, animation, group, keyframes, query, sequence, stagger, state, style, transition, trigger, useAnimation, NoopAnimationPlayer, ɵPRE_STYLE, AnimationGroupPlayer as ɵAnimationGroupPlayer };
+export { AUTO_STYLE, AnimationBuilder, AnimationFactory, NoopAnimationPlayer, animate, animateChild, animation, group, keyframes, query, sequence, stagger, state, style, transition, trigger, useAnimation, AnimationGroupPlayer as ɵAnimationGroupPlayer, ɵPRE_STYLE };
 //# sourceMappingURL=animations.js.map
