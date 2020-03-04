0d5aa7d610351fee8cfef72d5ace12d68fec50f5
Andrew Scott <atscott01@gmail.com>
2020-03-04 19:17:07 +0000

    1b8a66893e Revert "build(docs-infra): use local version of Zone.js when testing against local packages (#35780)" (#35857)

--- fesm2015/browser.js
+++ fesm2015/browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+62.sha-2e728f7
+ * @license Angular v9.1.0-next.2+71.sha-1b8a668
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -367,63 +367,6 @@ function hypenatePropsObject(object) {
         newObj[newProp] = object[prop];
     }));
     return newObj;
-}
-/**
- * Returns the computed style for the provided property on the provided element.
- *
- * This function uses `window.getComputedStyle` internally to determine the
- * style value for the element. Firefox doesn't support reading the shorthand
- * forms of margin/padding and for this reason this function needs to account
- * for that.
- * @param {?} element
- * @param {?} prop
- * @return {?}
- */
-function computeStyle(element, prop) {
-    /** @type {?} */
-    const styles = window.getComputedStyle(element);
-    // this is casted to any because the `CSSStyleDeclaration` type is a fixed
-    // set of properties and `prop` is a dynamic reference to a property within
-    // the `CSSStyleDeclaration` list.
-    /** @type {?} */
-    let value = getComputedValue(styles, (/** @type {?} */ (prop)));
-    // Firefox returns empty string values for `margin` and `padding` properties
-    // when extracted using getComputedStyle (see similar issue here:
-    // https://github.com/jquery/jquery/issues/3383). In this situation
-    // we want to emulate the value that is returned by creating the top,
-    // right, bottom and left properties as individual style lookups.
-    if (value.length === 0 && (prop === 'margin' || prop === 'padding')) {
-        /** @type {?} */
-        const t = getComputedValue(styles, (/** @type {?} */ ((prop + 'Top'))));
-        /** @type {?} */
-        const r = getComputedValue(styles, (/** @type {?} */ ((prop + 'Right'))));
-        /** @type {?} */
-        const b = getComputedValue(styles, (/** @type {?} */ ((prop + 'Bottom'))));
-        /** @type {?} */
-        const l = getComputedValue(styles, (/** @type {?} */ ((prop + 'Left'))));
-        // reconstruct the padding/margin value as `top right bottom left`
-        // we `trim()` the value because if all of the values above are
-        // empty string values then we would like the return value to
-        // also be an empty string.
-        value = `${t} ${r} ${b} ${l}`.trim();
-    }
-    return value;
-}
-/**
- * Reads and returns the provided property style from the provided styles collection.
- *
- * This function is useful because it will return an empty string in the
- * event that the value obtained from the styles collection is a non-string
- * value (which is usually the case if the `styles` object is mocked out).
- * @template K
- * @param {?} styles
- * @param {?} prop
- * @return {?}
- */
-function getComputedValue(styles, prop) {
-    /** @type {?} */
-    const value = styles[prop];
-    return typeof value === 'string' ? value : '';
 }
 
 /**
@@ -1039,6 +982,14 @@ function visitDslNode(visitor, node, context) {
         default:
             throw new Error(`Unable to resolve animation metadata node #${node.type}`);
     }
+}
+/**
+ * @param {?} element
+ * @param {?} prop
+ * @return {?}
+ */
+function computeStyle(element, prop) {
+    return ((/** @type {?} */ (window.getComputedStyle(element))))[prop];
 }
 
 /**
@@ -8076,7 +8027,7 @@ class CssKeyframesDriver {
      * @return {?}
      */
     computeStyle(element, prop, defaultValue) {
-        return computeStyle(element, prop);
+        return (/** @type {?} */ (((/** @type {?} */ (window.getComputedStyle(element))))[prop]));
     }
     /**
      * @param {?} element
@@ -8620,7 +8571,7 @@ class WebAnimationsDriver {
      * @return {?}
      */
     computeStyle(element, prop, defaultValue) {
-        return computeStyle(element, prop);
+        return (/** @type {?} */ (((/** @type {?} */ (window.getComputedStyle(element))))[prop]));
     }
     /**
      * @param {?} supported
