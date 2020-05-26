40817a190587ca89201b0d5e6fcfecbdc168de19
Matias Niemelä <matias@yearofmoo.com>
2020-05-26 21:00:43 +0000

    a62c1c441c build: migrate ng-dev config to .ng-dev directory (#37142)

--- bundles/animations-browser-testing.umd.js
+++ bundles/animations-browser-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+15.sha-ecce994
+ * @license Angular v10.0.0-rc.0+19.sha-a62c1c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -348,7 +348,7 @@
 
     /**
      * @license
-     * Copyright Google Inc. All Rights Reserved.
+     * Copyright Google LLC All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -356,7 +356,7 @@
 
     /**
      * @license
-     * Copyright Google Inc. All Rights Reserved.
+     * Copyright Google LLC All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -364,7 +364,7 @@
 
     /**
      * @license
-     * Copyright Google Inc. All Rights Reserved.
+     * Copyright Google LLC All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
--- fesm2015/browser/testing.js
+++ fesm2015/browser/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+15.sha-ecce994
+ * @license Angular v10.0.0-rc.0+19.sha-a62c1c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { ɵvalidateStyleProperty, ɵmatchesElement, ɵcontainsElement, ɵinvokeQ
 
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
@@ -119,7 +119,7 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
 
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
@@ -127,7 +127,7 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
 
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
@@ -135,7 +135,7 @@ class MockAnimationPlayer extends NoopAnimationPlayer {
 
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
