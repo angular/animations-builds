9c812f1e32b07b687564f69da78847c357e95881
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:24:11 +0000

    bfa197f564 fix(animations): replace copy of query selector node-list from "spread" to "for" (#39646)

--- fesm2015/browser.js
+++ fesm2015/browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+91.sha-5684ac5
+ * @license Angular v11.1.0-next.0+92.sha-bfa197f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -159,7 +159,17 @@ if (_isNode || typeof Element !== 'undefined') {
     _query = (element, selector, multi) => {
         let results = [];
         if (multi) {
-            results.push(...element.querySelectorAll(selector));
+            // DO NOT REFACTOR TO USE SPREAD SYNTAX.
+            // For element queries that return sufficiently large NodeList objects,
+            // using spread syntax to populate the results array causes a RangeError
+            // due to the call stack limit being reached. `Array.from` can not be used
+            // as well, since NodeList is not iterable in IE 11, see
+            // https://developer.mozilla.org/en-US/docs/Web/API/NodeList
+            // More info is available in #38551.
+            const elems = element.querySelectorAll(selector);
+            for (let i = 0; i < elems.length; i++) {
+                results.push(elems[i]);
+            }
         }
         else {
             const elm = element.querySelector(selector);
