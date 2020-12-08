5d5d4a9ce492b9bf331c3e8ad84ddd435e636eeb
Misko Hevery <misko@hevery.com>
2020-12-08 17:47:54 +0000

    8ab1231908 docs(animations): a small typo (#37164)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+42.sha-5631daa
+ * @license Angular v11.0.3+54.sha-8ab1231
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -275,8 +275,8 @@ function trigger(name, definitions) {
  * ```typescript
  * animate(500, keyframes(
  *  [
- *   style({ background: "blue" })),
- *   style({ background: "red" }))
+ *   style({ background: "blue" }),
+ *   style({ background: "red" })
  *  ])
  * ```
  *
