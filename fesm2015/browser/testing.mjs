36b9d3e1c95500a5ad6b7dbfc6fb598ed0dc6004
Andrew Kushnir <akushnir@google.com>
2021-10-18 16:50:13 +0000

    25b9c8cc5a docs: update getting started links to provide a more consistent user experience (#43872)

--- fesm2015/browser/testing.mjs
+++ fesm2015/browser/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+67.sha-efeb5ad.with-local-changes
+ * @license Angular v13.0.0-next.14+71.sha-25b9c8c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
