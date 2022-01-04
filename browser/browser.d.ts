fdb756e8ebc91d7a10bc66f5b9e0fd9c27b30ab1
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:26:05 +0000

    756761d534 fix(language-service): revert the test files for Ivy (#44528)

--- browser/browser.d.ts
+++ browser/browser.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+13.sha-7cb2999.with-local-changes
+ * @license Angular v13.2.0-next.1+12.sha-756761d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
