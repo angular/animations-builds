493a09e874df998182197a88c452a02ad27d382d
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 18:47:32 +0000

    4307b82058 refactor(common): make the error messages tree shakable (#44663)

--- animations.d.ts
+++ animations.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+16.sha-35345b6.with-local-changes
+ * @license Angular v13.2.0-next.2+18.sha-4307b82.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
