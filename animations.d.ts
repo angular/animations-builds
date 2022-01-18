81065e3adc9e64fa5124fd1e2bc0d8bad72dd6c6
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 17:40:34 +0000

    35345b607c build: update angular (#44728)

--- animations.d.ts
+++ animations.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+17.sha-ccb73c0.with-local-changes
+ * @license Angular v13.2.0-next.2+16.sha-35345b6.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
