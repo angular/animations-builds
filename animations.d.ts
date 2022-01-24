6f0afee85bc1d561cfbb11cd34475c31e0147c98
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:09:31 +0000

    c9fcc4f617 test(animations): Add bundling symbol test for animations package (#44809)

--- animations.d.ts
+++ animations.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+59.sha-531d1cf.with-local-changes
+ * @license Angular v13.2.0-next.2+63.sha-c9fcc4f.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
