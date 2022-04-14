08fcef92d2b56b18fae52d5e68c0e20f835361e9
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:16:21 +0000

    e0a2248b32 feat(forms): Add a FormRecord type. (#45607)

--- animations.d.ts
+++ animations.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.13+2.sha-000363e
+ * @license Angular v14.0.0-next.13+4.sha-e0a2248
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2020/browser.mjs
+++ fesm2020/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.13+2.sha-000363e
+ * @license Angular v14.0.0-next.13+4.sha-e0a2248
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -580,9 +580,9 @@ class NoopAnimationDriver {
         return new NoopAnimationPlayer(duration, delay);
     }
 }
-NoopAnimationDriver.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.13+2.sha-000363e", ngImport: i0, type: NoopAnimationDriver, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-NoopAnimationDriver.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.0.0-next.13+2.sha-000363e", ngImport: i0, type: NoopAnimationDriver });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.13+2.sha-000363e", ngImport: i0, type: NoopAnimationDriver, decorators: [{
+NoopAnimationDriver.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.13+4.sha-e0a2248", ngImport: i0, type: NoopAnimationDriver, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+NoopAnimationDriver.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.0.0-next.13+4.sha-e0a2248", ngImport: i0, type: NoopAnimationDriver });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.13+4.sha-e0a2248", ngImport: i0, type: NoopAnimationDriver, decorators: [{
             type: Injectable
         }] });
 /**
