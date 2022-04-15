afacf031c4766dc16e699fe596ba06220d31d0a5
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:55:22 +0000

    68357104c9 build: lock file maintenance (#45457)

--- fesm2020/browser.mjs
+++ fesm2020/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.13+18.sha-0b64072
+ * @license Angular v14.0.0-next.13+19.sha-6835710
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -580,9 +580,9 @@ class NoopAnimationDriver {
         return new NoopAnimationPlayer(duration, delay);
     }
 }
-NoopAnimationDriver.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.13+18.sha-0b64072", ngImport: i0, type: NoopAnimationDriver, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-NoopAnimationDriver.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.0.0-next.13+18.sha-0b64072", ngImport: i0, type: NoopAnimationDriver });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.13+18.sha-0b64072", ngImport: i0, type: NoopAnimationDriver, decorators: [{
+NoopAnimationDriver.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.13+19.sha-6835710", ngImport: i0, type: NoopAnimationDriver, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+NoopAnimationDriver.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.0.0-next.13+19.sha-6835710", ngImport: i0, type: NoopAnimationDriver });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.13+19.sha-6835710", ngImport: i0, type: NoopAnimationDriver, decorators: [{
             type: Injectable
         }] });
 /**
