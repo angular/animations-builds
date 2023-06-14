d3cb9f2d3af684ab43e34a086c3b49535f14b6d4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 09:14:28 +0000

    f199305db6 docs(platform-server): Make links out of @see tags (#50110)

--- fesm2022/animations.mjs
+++ fesm2022/animations.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-1ad4d55
+ * @license Angular v16.2.0-next.0+sha-f199305
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -922,9 +922,9 @@ function scheduleMicroTask(cb) {
  * Used internally when animations are disabled, to avoid
  * checking for the null case when an animation player is expected.
  *
- * @see `animate()`
- * @see `AnimationPlayer`
- * @see `GroupPlayer`
+ * @see {@link animate}
+ * @see {@link AnimationPlayer}
+ * @see {@link ɵAnimationGroupPlayer AnimationGroupPlayer}
  *
  * @publicApi
  */
@@ -1019,8 +1019,8 @@ class NoopAnimationPlayer {
  * A programmatic controller for a group of reusable animations.
  * Used internally to control animations.
  *
- * @see `AnimationPlayer`
- * @see `{@link animations/group group()}`
+ * @see {@link AnimationPlayer}
+ * @see {@link animations/group group}
  *
  */
 class AnimationGroupPlayer {
