6cfb1a6d3a7dded52e8f2d39018d0213789fd22e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:33:25 +0000

    7620f50 refactor(core): node removal notifies scheduler only when animations are enabled (#53857)

--- browser/index.d.ts
+++ browser/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-62ad2f0
+ * @license Angular v17.1.0+sha-7620f50
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16,6 +16,7 @@ import { RendererFactory2 } from '@angular/core';
 import { RendererStyleFlags2 } from '@angular/core';
 import type { RendererType2 } from '@angular/core';
 import { ɵAnimationRendererType } from '@angular/core';
+import { ɵChangeDetectionScheduler } from '@angular/core';
 import { ɵStyleData } from '@angular/animations';
 import { ɵStyleDataMap } from '@angular/animations';
 
@@ -155,7 +156,7 @@ export declare class ɵAnimationEngine {
     private _timelineEngine;
     private _triggerCache;
     onRemovalComplete: (element: any, context: any) => void;
-    constructor(doc: Document, _driver: AnimationDriver, _normalizer: ɵAnimationStyleNormalizer);
+    constructor(doc: Document, _driver: AnimationDriver, _normalizer: ɵAnimationStyleNormalizer, scheduler: ɵChangeDetectionScheduler | null);
     registerTrigger(componentId: string, namespaceId: string, hostElement: any, name: string, metadata: AnimationTriggerMetadata): void;
     register(namespaceId: string, hostElement: any): void;
     destroy(namespaceId: string, context: any): void;
@@ -237,7 +238,7 @@ export declare function ɵcamelCaseToDashCase(input: string): string;
 
 export declare function ɵcontainsElement(elm1: any, elm2: any): boolean;
 
-export declare function ɵcreateEngine(type: 'animations' | 'noop', doc: Document): ɵAnimationEngine;
+export declare function ɵcreateEngine(type: 'animations' | 'noop', doc: Document, scheduler: ɵChangeDetectionScheduler | null): ɵAnimationEngine;
 
 export declare function ɵgetParentElement(element: any): unknown | null;
 
