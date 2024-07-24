6ed1d12fb8c60dfd854d287032f42021b708d8ba
Andrew Scott <atscott@google.com>
2024-07-24 17:29:44 +0000

    a7b973e docs(docs-infra): Use shiki for code highlighting (#57059)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-d5fbe11
+ * @license Angular v18.2.0-next.1+sha-a7b973e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1368,7 +1368,7 @@ export declare function style(tokens: '*' | {
  *  - Transition animations applied based on
  *    the trigger's expression value
  *
- *   ```HTML
+ *   ```html
  *   <div [@myAnimationTrigger]="myStatusExp">
  *    ...
  *   </div>
@@ -1385,7 +1385,7 @@ export declare function style(tokens: '*' | {
  *  - Transition animations applied based on custom logic dependent
  *    on the trigger's expression value and provided parameters
  *
- *    ```HTML
+ *    ```html
  *    <div [@myAnimationTrigger]="{
  *     value: stepName,
  *     params: { target: currentTarget }
