bdbe4abb5e4b2f47f634559abccba6013f47c96d
Miles Malerba <mmalerba@google.com>
2025-10-02 15:03:04 +0000

    595c668 docs: update website URL in license banners  (#64183)

--- types/_animation_player-chunk.d.ts
+++ types/_animation_player-chunk.d.ts
@@ -1,6 +1,6 @@
 /**
- * @license Angular v21.0.0-next.6+sha-50866b6
- * (c) 2010-2025 Google LLC. https://angular.io/
+ * @license Angular v21.0.0-next.6+sha-595c668
+ * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
