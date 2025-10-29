548b766f3d481178b6838ed4214e745006570c68
Andrew Scott <atscott@google.com>
2025-10-29 21:01:25 +0000

    60cc3cb refactor(core): Move patch toggles inside functions to allow tree shaking

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-366392b
+ * @license Angular v21.0.0-rc.0+sha-60cc3cb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-366392b",
+    version: "21.0.0-rc.0+sha-60cc3cb",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-366392b",
+    version: "21.0.0-rc.0+sha-60cc3cb",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-366392b",
+  version: "21.0.0-rc.0+sha-60cc3cb",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
