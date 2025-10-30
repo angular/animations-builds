f4783c220e0c2b1d6e7b517d3fd83d7475a1c1e9
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:20:36 +0000

    90452f5 docs: update Angular Aria apis (#64804)

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-60cc3cb
+ * @license Angular v21.0.0-rc.0+sha-90452f5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-60cc3cb",
+    version: "21.0.0-rc.0+sha-90452f5",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-60cc3cb",
+    version: "21.0.0-rc.0+sha-90452f5",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-60cc3cb",
+  version: "21.0.0-rc.0+sha-90452f5",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
