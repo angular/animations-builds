ed674d1949cec22687308e561af4ae4fad533621
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 19:04:25 +0000

    e90f9e4 refactor(forms): tree-shake `ngControlStatusHost` and `ngGroupStatusHost`

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-3bed9f0
+ * @license Angular v21.0.0-rc.1+sha-e90f9e4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-3bed9f0",
+    version: "21.0.0-rc.1+sha-e90f9e4",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-3bed9f0",
+    version: "21.0.0-rc.1+sha-e90f9e4",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.1+sha-3bed9f0",
+  version: "21.0.0-rc.1+sha-e90f9e4",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
