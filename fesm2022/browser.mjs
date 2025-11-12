7c7dd2d31f22a3c7c3ab4b2ba5227f3d5d5aff4e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 19:38:54 +0000

    129e16d docs: correct code block language in error handling and signals interop

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-18cc260
+ * @license Angular v21.0.0-rc.2+sha-129e16d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-18cc260",
+    version: "21.0.0-rc.2+sha-129e16d",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-18cc260",
+    version: "21.0.0-rc.2+sha-129e16d",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-18cc260",
+  version: "21.0.0-rc.2+sha-129e16d",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
