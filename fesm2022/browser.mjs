67181979c7f6491ea851bc34cb815682d901832f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:16:59 +0000

    4b0f208 ci: use hardcoded global approvers group

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-482e137
+ * @license Angular v21.0.0-rc.2+sha-4b0f208
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-482e137",
+    version: "21.0.0-rc.2+sha-4b0f208",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-482e137",
+    version: "21.0.0-rc.2+sha-4b0f208",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-482e137",
+  version: "21.0.0-rc.2+sha-4b0f208",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
