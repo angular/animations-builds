a5e9fe028fbf1938a14dfb59bae3eca69ffb00aa
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 22:48:39 +0000

    e7a6e31 fix(devtools): refine when signal graph button is shown

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c757fd6
+ * @license Angular v21.1.0-next.0+sha-e7a6e31
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-c757fd6",
+    version: "21.1.0-next.0+sha-e7a6e31",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-c757fd6",
+    version: "21.1.0-next.0+sha-e7a6e31",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-c757fd6",
+  version: "21.1.0-next.0+sha-e7a6e31",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
