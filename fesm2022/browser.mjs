61b4b0db2b35f6ecc0d667dd6fbc24dca5bc1ed0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 01:04:56 +0000

    7a910e6 docs: add signal forms comparison guide

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-47eb77f
+ * @license Angular v21.1.0-next.0+sha-7a910e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-47eb77f",
+    version: "21.1.0-next.0+sha-7a910e6",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-47eb77f",
+    version: "21.1.0-next.0+sha-7a910e6",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-47eb77f",
+  version: "21.1.0-next.0+sha-7a910e6",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
