e6d74814d0e12f22e24a650db1ed54c6385daad7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:22:40 +0000

    e69855a docs: aria grid replace overview example with a playful one

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-17b7d56
+ * @license Angular v21.0.0-rc.3+sha-e69855a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.3+sha-17b7d56",
+    version: "21.0.0-rc.3+sha-e69855a",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.3+sha-17b7d56",
+    version: "21.0.0-rc.3+sha-e69855a",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.3+sha-17b7d56",
+  version: "21.0.0-rc.3+sha-e69855a",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
