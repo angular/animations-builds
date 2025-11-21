a5794351701ddad324f4fd12dacffb97b04197b7
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 21:38:02 +0000

    38a354f docs: Adds signal type checking documentation

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f884c18
+ * @license Angular v21.1.0-next.0+sha-38a354f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-f884c18",
+    version: "21.1.0-next.0+sha-38a354f",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-f884c18",
+    version: "21.1.0-next.0+sha-38a354f",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-f884c18",
+  version: "21.1.0-next.0+sha-38a354f",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
