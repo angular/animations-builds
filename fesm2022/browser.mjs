54104f6991d1a959ccd8315f3c438fc4c1ca7563
kirjs <kirts@google.com>
2025-11-21 15:59:53 +0000

    1cdcd5a docs: fix incorrect Signal Forms link

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-053bf98
+ * @license Angular v21.0.0+sha-1cdcd5a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-053bf98",
+    version: "21.0.0+sha-1cdcd5a",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-053bf98",
+    version: "21.0.0+sha-1cdcd5a",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-053bf98",
+  version: "21.0.0+sha-1cdcd5a",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
