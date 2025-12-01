4c2431aa119c4d5f9b6bd7fb87aaacecf584fb88
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:24:02 +0000

    8ab7594 docs: format typescript examples and replace <docs-code> with fenced code blocks

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-ed32e8c
+ * @license Angular v21.0.2+sha-8ab7594
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-ed32e8c",
+    version: "21.0.2+sha-8ab7594",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-ed32e8c",
+    version: "21.0.2+sha-8ab7594",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.2+sha-ed32e8c",
+  version: "21.0.2+sha-8ab7594",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
