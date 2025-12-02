7834924c23b01387a8c9a616d0872b2f66717a58
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 09:41:50 +0000

    893fbdf docs: update syntax highlighting in component documentation

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-706d762
+ * @license Angular v21.0.2+sha-893fbdf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-706d762",
+    version: "21.0.2+sha-893fbdf",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-706d762",
+    version: "21.0.2+sha-893fbdf",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.2+sha-706d762",
+  version: "21.0.2+sha-893fbdf",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
