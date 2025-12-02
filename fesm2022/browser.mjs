3486c2f2714009fa73f5861665d93f1aa0475219
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 18:16:17 +0000

    c2a8934 Revert "refactor(router): compress synchronous end to router navigation to single operator"

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-5a80a48
+ * @license Angular v21.0.2+sha-c2a8934
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-5a80a48",
+    version: "21.0.2+sha-c2a8934",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-5a80a48",
+    version: "21.0.2+sha-c2a8934",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.2+sha-5a80a48",
+  version: "21.0.2+sha-c2a8934",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
