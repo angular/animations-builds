81740fdec5fe3828eb46c996605607e26de07fbf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 09:09:26 +0000

    8e837cc docs: rename visibleRegion to region

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-e95d2e3
+ * @license Angular v21.0.2+sha-8e837cc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-e95d2e3",
+    version: "21.0.2+sha-8e837cc",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-e95d2e3",
+    version: "21.0.2+sha-8e837cc",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.2+sha-e95d2e3",
+  version: "21.0.2+sha-8e837cc",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
--- types/browser-testing.d.ts
+++ types/browser-testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-e95d2e3
+ * @license Angular v21.0.2+sha-8e837cc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
