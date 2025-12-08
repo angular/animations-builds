cc993a3214820b3d4116d387a54ae60db0810926
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 16:47:32 +0000

    d2ac93d build: format manifest files after updating its version

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-789f91b
+ * @license Angular v21.1.0-next.1+sha-d2ac93d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.1+sha-789f91b",
+    version: "21.1.0-next.1+sha-d2ac93d",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.1+sha-789f91b",
+    version: "21.1.0-next.1+sha-d2ac93d",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.1+sha-789f91b",
+  version: "21.1.0-next.1+sha-d2ac93d",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
