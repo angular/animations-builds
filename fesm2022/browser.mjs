bc5a2b5dbce83fd7a329f68d87216a7d3979d3d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 20:36:00 +0000

    de6dc04 docs: add `'new'` to page `'Side effects for non-reactive APIs'`

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-497c0e3
+ * @license Angular v21.0.3+sha-de6dc04
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-497c0e3",
+    version: "21.0.3+sha-de6dc04",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-497c0e3",
+    version: "21.0.3+sha-de6dc04",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.3+sha-497c0e3",
+  version: "21.0.3+sha-de6dc04",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
