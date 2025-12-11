a5530b709e9b5e3601c0b720965ee788ba4085e1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:45:25 +0000

    5a21465 release: cut the v21.0.5 release

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.4+sha-4fd2b72
+ * @license Angular v21.0.5+sha-5a21465
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.4+sha-4fd2b72",
+    version: "21.0.5+sha-5a21465",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.4+sha-4fd2b72",
+    version: "21.0.5+sha-5a21465",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.4+sha-4fd2b72",
+  version: "21.0.5+sha-5a21465",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
