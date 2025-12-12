ac1c03435669195c23a71de0ced0329c129eb77c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 18:09:42 +0000

    d6b02a7 docs: update cross-repo adev docs

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-27bb376
+ * @license Angular v21.0.5+sha-d6b02a7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.5+sha-27bb376",
+    version: "21.0.5+sha-d6b02a7",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.5+sha-27bb376",
+    version: "21.0.5+sha-d6b02a7",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.5+sha-27bb376",
+  version: "21.0.5+sha-d6b02a7",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
