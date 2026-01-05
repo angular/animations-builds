5b03c92cf0ae7f93217c36198bc2f98d31f26bba
kirjs <kirts@google.com>
2026-01-05 17:20:36 +0000

    c1f22a3 docs: Add form control state management and event options

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-5e6a9ba
+ * @license Angular v21.0.6+sha-c1f22a3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-5e6a9ba",
+    version: "21.0.6+sha-c1f22a3",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-5e6a9ba",
+    version: "21.0.6+sha-c1f22a3",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.6+sha-5e6a9ba",
+  version: "21.0.6+sha-c1f22a3",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
