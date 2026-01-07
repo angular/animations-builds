23f7c9257cc97b4a1c09bb6ce886a72d42c4bcb6
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 17:35:53 +0000

    85ce5f3 docs: update copyright year

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,6 +1,6 @@
 /**
- * @license Angular v21.1.0-next.4+sha-97fd1de
- * (c) 2010-2025 Google LLC. https://angular.dev/
+ * @license Angular v21.1.0-next.4+sha-85ce5f3
+ * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.4+sha-97fd1de",
+    version: "21.1.0-next.4+sha-85ce5f3",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.4+sha-97fd1de",
+    version: "21.1.0-next.4+sha-85ce5f3",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.4+sha-97fd1de",
+  version: "21.1.0-next.4+sha-85ce5f3",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
