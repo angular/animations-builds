42c75b7f18b1c5c396818d3e53f773e07f8a9691
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 15:51:19 +0000

    baee12d Revert "docs: remove incorrectly listed operators in "Supported operators" table (Expression Syntax Guide) (#62092)"

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-696adbc
+ * @license Angular v21.0.8+sha-baee12d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-696adbc",
+    version: "21.0.8+sha-baee12d",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-696adbc",
+    version: "21.0.8+sha-baee12d",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.8+sha-696adbc",
+  version: "21.0.8+sha-baee12d",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
