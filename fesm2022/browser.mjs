eea490155242f42baebc13a711beb78f7658fdb1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:53:46 +0000

    74e99fa docs(docs-infra): prevent from showing duplicate usage blocks in functions API docs

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-08d0ac7
+ * @license Angular v21.0.8+sha-74e99fa
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-08d0ac7",
+    version: "21.0.8+sha-74e99fa",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-08d0ac7",
+    version: "21.0.8+sha-74e99fa",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.8+sha-08d0ac7",
+  version: "21.0.8+sha-74e99fa",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
