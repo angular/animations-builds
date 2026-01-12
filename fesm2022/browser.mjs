e15117b4de741e88750dbbae946216ab4656a0dc
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:22:28 +0000

    5b0e705 docs: improve separation between enter and leave example

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-615a77b
+ * @license Angular v21.0.8+sha-5b0e705
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-615a77b",
+    version: "21.0.8+sha-5b0e705",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-615a77b",
+    version: "21.0.8+sha-5b0e705",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.8+sha-615a77b",
+  version: "21.0.8+sha-5b0e705",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
