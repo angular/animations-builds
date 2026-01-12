076e194ace904de722b77fe77fb02db1097ac529
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 21:57:01 +0000

    5326333 fix(forms): Ensure the control instruction comes after the other bindings

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-5ad2e1b
+ * @license Angular v21.1.0-rc.0+sha-5326333
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-rc.0+sha-5ad2e1b",
+    version: "21.1.0-rc.0+sha-5326333",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-rc.0+sha-5ad2e1b",
+    version: "21.1.0-rc.0+sha-5326333",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-rc.0+sha-5ad2e1b",
+  version: "21.1.0-rc.0+sha-5326333",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
