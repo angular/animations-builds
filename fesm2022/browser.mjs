94e37be55ea5f8a706d8d3c77df504c46e83bb57
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 16:47:18 +0000

    8f4cc84 docs: update cross-repo adev docs

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-33de839
+ * @license Angular v21.0.8+sha-8f4cc84
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-33de839",
+    version: "21.0.8+sha-8f4cc84",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-33de839",
+    version: "21.0.8+sha-8f4cc84",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.8+sha-33de839",
+  version: "21.0.8+sha-8f4cc84",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
