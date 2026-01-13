2177db6b68718bc2e78ffa9a1882d314e5d7587e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 16:42:29 +0000

    e8f8ac2 build: remove `@angular-devkit/architect-cli` from dev dependencies

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-69da639
+ * @license Angular v21.2.0-next.0+sha-e8f8ac2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-69da639",
+    version: "21.2.0-next.0+sha-e8f8ac2",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-69da639",
+    version: "21.2.0-next.0+sha-e8f8ac2",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-69da639",
+  version: "21.2.0-next.0+sha-e8f8ac2",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
