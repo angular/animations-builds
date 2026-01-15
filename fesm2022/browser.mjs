7c15dda46018639edac9a4ec350edf08f61071aa
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 17:03:28 +0000

    c0446da docs(docs-infra): prevent heading from linking symbols

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-ae4e2e0
+ * @license Angular v21.1.0+sha-c0446da
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-ae4e2e0",
+    version: "21.1.0+sha-c0446da",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-ae4e2e0",
+    version: "21.1.0+sha-c0446da",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0+sha-ae4e2e0",
+  version: "21.1.0+sha-c0446da",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
