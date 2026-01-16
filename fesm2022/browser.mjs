00f22461e44fa99b95e0fa9105b73b5bd2a99352
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 17:23:57 +0000

    54fc393 build: update dependency @csstools/css-calc to v3

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-d9c980a
+ * @license Angular v21.2.0-next.0+sha-54fc393
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-d9c980a",
+    version: "21.2.0-next.0+sha-54fc393",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-d9c980a",
+    version: "21.2.0-next.0+sha-54fc393",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-d9c980a",
+  version: "21.2.0-next.0+sha-54fc393",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
