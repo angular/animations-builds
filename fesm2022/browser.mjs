329b1f286f8d95e6e4bee9db23495fefb4ee988e
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-20 17:40:11 +0000

    a88f353 fix(docs-infra): support providing links to multiple symbols within a single line of a code block

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-5d46554-with-local-changes
+ * @license Angular v21.2.0-next.0+sha-a88f353
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-5d46554-with-local-changes",
+    version: "21.2.0-next.0+sha-a88f353",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-5d46554-with-local-changes",
+    version: "21.2.0-next.0+sha-a88f353",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-5d46554-with-local-changes",
+  version: "21.2.0-next.0+sha-a88f353",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
