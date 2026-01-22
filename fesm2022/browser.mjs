d69a8599736af84da3e94e30025530bc14fddea2
GitHub <noreply@github.com>
2026-01-22 18:36:40 +0000

    27eeff9 release: bump VSCode extension version to 21.1.1 (#66714)

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-f577f8c
+ * @license Angular v21.1.1+sha-27eeff9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-f577f8c",
+    version: "21.1.1+sha-27eeff9",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-f577f8c",
+    version: "21.1.1+sha-27eeff9",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-f577f8c",
+  version: "21.1.1+sha-27eeff9",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
