ff94378d7ec72d04748c92cff53ca6dca3cde2c5
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:10:05 +0000

    c9043fb refactor(compiler-cli): remove deprecated signature usage

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-61dbdee
+ * @license Angular v21.1.1+sha-c9043fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-61dbdee",
+    version: "21.1.1+sha-c9043fb",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-61dbdee",
+    version: "21.1.1+sha-c9043fb",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-61dbdee",
+  version: "21.1.1+sha-c9043fb",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
