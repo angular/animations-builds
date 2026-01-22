c638bb33714fb7b76aef4b562590d457d3b8e618
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:17:40 +0000

    e4c9ff7 build: Update prettier to 3.8

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-c9043fb
+ * @license Angular v21.1.1+sha-e4c9ff7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-c9043fb",
+    version: "21.1.1+sha-e4c9ff7",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-c9043fb",
+    version: "21.1.1+sha-e4c9ff7",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-c9043fb",
+  version: "21.1.1+sha-e4c9ff7",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
