4a24b1fb6537bd60eaf9816d8ef9970be1a3e0ac
Alan Agius <alan.agius4@gmail.com>
2026-01-28 09:56:27 +0000

    4123ebf release: bump Angular DevTools version to 1.9.0

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-01bfb83
+ * @license Angular v21.2.0-next.0+sha-4123ebf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-01bfb83",
+    version: "21.2.0-next.0+sha-4123ebf",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-01bfb83",
+    version: "21.2.0-next.0+sha-4123ebf",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-01bfb83",
+  version: "21.2.0-next.0+sha-4123ebf",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
