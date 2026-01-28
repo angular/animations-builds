ff173e2efc6021fcaaef8ea518b5cd13e0297749
Andrew Scott <atscott01@gmail.com>
2026-01-28 21:00:07 +0000

    d072791 refactor(core): remove unused restriction parameter

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-5a6fefb
+ * @license Angular v21.2.0-next.1+sha-d072791
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-5a6fefb",
+    version: "21.2.0-next.1+sha-d072791",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-5a6fefb",
+    version: "21.2.0-next.1+sha-d072791",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.1+sha-5a6fefb",
+  version: "21.2.0-next.1+sha-d072791",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
