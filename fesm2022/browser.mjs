970e67f71dbd0a72b5798e553b06a159b6279a28
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:12:06 +0000

    0a4c647 refactor(core): Remove unsued properties in differ factories

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-cfe7340
+ * @license Angular v21.1.2+sha-0a4c647
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.2+sha-cfe7340",
+    version: "21.1.2+sha-0a4c647",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.2+sha-cfe7340",
+    version: "21.1.2+sha-0a4c647",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.2+sha-cfe7340",
+  version: "21.1.2+sha-0a4c647",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
