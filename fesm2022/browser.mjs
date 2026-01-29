1b1b4d253c13280eec94c2be99a6a838e02f55ad
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:15:12 +0000

    907a94d feat(router): Update `IsActiveMatchOptions` APIs to accept a Partial

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-0e04233
+ * @license Angular v21.2.0-next.1+sha-907a94d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-0e04233",
+    version: "21.2.0-next.1+sha-907a94d",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-0e04233",
+    version: "21.2.0-next.1+sha-907a94d",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.1+sha-0e04233",
+  version: "21.2.0-next.1+sha-907a94d",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
