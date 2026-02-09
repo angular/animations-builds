8fcef3e8f35aaf97bd4663be8e09f5d5307978ed
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 16:02:40 +0000

    4b3b149 refactor(benchpress): remove zone-based testing utilities

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-63d828a
+ * @license Angular v21.2.0-next.2+sha-4b3b149
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.2+sha-63d828a",
+    version: "21.2.0-next.2+sha-4b3b149",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.2+sha-63d828a",
+    version: "21.2.0-next.2+sha-4b3b149",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.2+sha-63d828a",
+  version: "21.2.0-next.2+sha-4b3b149",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
