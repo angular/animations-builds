d52078f27e13b1ed6b25b82aa9fdb563cf4cd300
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 23:27:20 +0000

    c235b57 ci: update pullapprove for manual_api docs

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-24b39ab
+ * @license Angular v21.1.5+sha-c235b57
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.5+sha-24b39ab",
+    version: "21.1.5+sha-c235b57",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.5+sha-24b39ab",
+    version: "21.1.5+sha-c235b57",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.5+sha-24b39ab",
+  version: "21.1.5+sha-c235b57",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
