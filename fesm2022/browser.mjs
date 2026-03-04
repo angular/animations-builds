eb837e5b7989e1888f4348b1e4ded7ba53e761f5
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 18:23:46 +0000

    6d7446b refactor(core): account for shadow roots in animations

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-c25c84d
+ * @license Angular v21.2.0+sha-6d7446b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-c25c84d",
+    version: "21.2.0+sha-6d7446b",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-c25c84d",
+    version: "21.2.0+sha-6d7446b",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0+sha-c25c84d",
+  version: "21.2.0+sha-6d7446b",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
