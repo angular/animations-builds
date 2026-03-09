82286e4f830bc57334a672b48a1593835907021a
Andrew Scott <atscott01@gmail.com>
2026-03-09 23:33:53 +0000

    bf93ff8 ci: fix exclusion of manual_api_docs

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-b918bed
+ * @license Angular v22.0.0-next.1+sha-bf93ff8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.1+sha-b918bed",
+    version: "22.0.0-next.1+sha-bf93ff8",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.1+sha-b918bed",
+    version: "22.0.0-next.1+sha-bf93ff8",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.1+sha-b918bed",
+  version: "22.0.0-next.1+sha-bf93ff8",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
