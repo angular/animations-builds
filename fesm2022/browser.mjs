722b6fc3c45a2c641d916dd6b570e5edeca27fd6
Andrew Scott <atscott01@gmail.com>
2026-03-11 17:09:02 +0000

    d45fced docs: remove duplicate aria-hidden attribute in angular aria ngTree example

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-ed48c41
+ * @license Angular v21.2.2+sha-d45fced
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.2+sha-ed48c41",
+    version: "21.2.2+sha-d45fced",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.2+sha-ed48c41",
+    version: "21.2.2+sha-d45fced",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.2+sha-ed48c41",
+  version: "21.2.2+sha-d45fced",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
