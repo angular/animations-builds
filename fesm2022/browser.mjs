94a90ef43866166007fef802c84048ae85e46211
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 19:07:08 +0000

    5b66252 docs: release notes for the v21.2.4 release

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.2+sha-78dea55
+ * @license Angular v22.0.0-next.2+sha-5b66252
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.2+sha-78dea55",
+    version: "22.0.0-next.2+sha-5b66252",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.2+sha-78dea55",
+    version: "22.0.0-next.2+sha-5b66252",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.2+sha-78dea55",
+  version: "22.0.0-next.2+sha-5b66252",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
