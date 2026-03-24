3557ae0b38d38f03ef19ab5f80bd2979a123ddf6
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 22:42:33 +0000

    050b14b docs: fix incorrect getHarnesses() method to getAllHarnesses and broken link

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-d9712e6
+ * @license Angular v22.0.0-next.4+sha-050b14b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.4+sha-d9712e6",
+    version: "22.0.0-next.4+sha-050b14b",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.4+sha-d9712e6",
+    version: "22.0.0-next.4+sha-050b14b",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.4+sha-d9712e6",
+  version: "22.0.0-next.4+sha-050b14b",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
