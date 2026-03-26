362ffadeb8ce563e0bc29b255714d9879b4c01cb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:40:54 +0000

    13f050d test: construct local `Date` objects to fix timezone flakiness

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-d8239c7
+ * @license Angular v21.2.6+sha-13f050d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.6+sha-d8239c7",
+    version: "21.2.6+sha-13f050d",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.6+sha-d8239c7",
+    version: "21.2.6+sha-13f050d",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.6+sha-d8239c7",
+  version: "21.2.6+sha-13f050d",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
