7e3771b45c0086eb1e506f85f2e7d35f84df4d04
Andrew Scott <atscott01@gmail.com>
2026-04-08 18:42:12 +0000

    6fee651 refactor(compiler-cli): decouple SymbolBuilder from BoundTarget and minimize adapter surface

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-c7518a4
+ * @license Angular v22.0.0-next.6+sha-6fee651
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.6+sha-c7518a4",
+    version: "22.0.0-next.6+sha-6fee651",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.6+sha-c7518a4",
+    version: "22.0.0-next.6+sha-6fee651",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.6+sha-c7518a4",
+  version: "22.0.0-next.6+sha-6fee651",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
