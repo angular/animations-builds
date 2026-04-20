b649dc5b37a0af3bf0320c93d0b192539d713ef2
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:28:34 +0000

    0d08d9c docs(docs-infra): guard sandbox reset before initialization in playground

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-c9215b3
+ * @license Angular v21.2.9+sha-0d08d9c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.9+sha-c9215b3",
+    version: "21.2.9+sha-0d08d9c",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.9+sha-c9215b3",
+    version: "21.2.9+sha-0d08d9c",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.9+sha-c9215b3",
+  version: "21.2.9+sha-0d08d9c",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
