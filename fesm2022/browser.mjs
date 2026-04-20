905877451380aee110030c14ca69b9454ad0825d
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:58:50 +0000

    e5b93ea docs: fix wording in reactive forms guide

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-ce883d9
+ * @license Angular v21.2.9+sha-e5b93ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.9+sha-ce883d9",
+    version: "21.2.9+sha-e5b93ea",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.9+sha-ce883d9",
+    version: "21.2.9+sha-e5b93ea",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.9+sha-ce883d9",
+  version: "21.2.9+sha-e5b93ea",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
