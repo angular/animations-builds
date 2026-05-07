6bce3d33d31265a5636ce1b6d2bc1336509aa66b
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 22:51:04 +0000

    609f930 docs: update cross-repo adev docs

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.11+sha-a3d4631
+ * @license Angular v22.0.0-next.11+sha-609f930
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.11+sha-a3d4631",
+    version: "22.0.0-next.11+sha-609f930",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.11+sha-a3d4631",
+    version: "22.0.0-next.11+sha-609f930",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.11+sha-a3d4631",
+  version: "22.0.0-next.11+sha-609f930",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
