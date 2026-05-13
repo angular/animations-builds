629bbf953c8ff417d1660a044d7be0a5a9d57c2d
Matthew Beck <me@mattrb.com>
2026-05-13 23:03:39 +0000

    0d55de8 refactor(core): mark resource() overloads as @publicApi 22.0

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-639a785
+ * @license Angular v22.0.0-next.12+sha-0d55de8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.12+sha-639a785",
+    version: "22.0.0-next.12+sha-0d55de8",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.12+sha-639a785",
+    version: "22.0.0-next.12+sha-0d55de8",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.12+sha-639a785",
+  version: "22.0.0-next.12+sha-0d55de8",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
--- types/browser-testing.d.ts
+++ types/browser-testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-639a785
+ * @license Angular v22.0.0-next.12+sha-0d55de8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
