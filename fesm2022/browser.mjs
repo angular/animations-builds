0719d394635e8955a6b06e213637af1bb0c349a0
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 18:28:57 +0000

    1d6e71d docs: clarify ngDoCheck invocation behavior with OnPush strategy

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-0c7f70e
+ * @license Angular v21.2.13+sha-1d6e71d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.13+sha-0c7f70e",
+    version: "21.2.13+sha-1d6e71d",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.13+sha-0c7f70e",
+    version: "21.2.13+sha-1d6e71d",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.13+sha-0c7f70e",
+  version: "21.2.13+sha-1d6e71d",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
