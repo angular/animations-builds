192fd99f407f6c9485a645853b7a5821456de7ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 19:29:30 +0000

    b25964b fix(vscode-extension): disable language server in untrusted workspaces

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-c08f22a
+ * @license Angular v22.0.0-rc.1+sha-b25964b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.1+sha-c08f22a",
+    version: "22.0.0-rc.1+sha-b25964b",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.1+sha-c08f22a",
+    version: "22.0.0-rc.1+sha-b25964b",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-rc.1+sha-c08f22a",
+  version: "22.0.0-rc.1+sha-b25964b",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
