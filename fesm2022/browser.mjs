017cd004acba56499842b76420f0e4908a787f01
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:35:45 +0000

    29ceeff docs: fix typos in source code comments

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-2b17b2d
+ * @license Angular v21.2.14+sha-29ceeff
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.14+sha-2b17b2d",
+    version: "21.2.14+sha-29ceeff",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.14+sha-2b17b2d",
+    version: "21.2.14+sha-29ceeff",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.14+sha-2b17b2d",
+  version: "21.2.14+sha-29ceeff",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
