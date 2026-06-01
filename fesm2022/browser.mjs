3a0cba161536e19f21b0904053c0253f091126de
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 16:41:13 +0000

    ec4f08b docs(docs-infra): simplify code block styles

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-5215f73
+ * @license Angular v22.1.0-next.0+sha-ec4f08b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-5215f73",
+    version: "22.1.0-next.0+sha-ec4f08b",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-5215f73",
+    version: "22.1.0-next.0+sha-ec4f08b",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.0+sha-5215f73",
+  version: "22.1.0-next.0+sha-ec4f08b",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
