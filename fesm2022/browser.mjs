6754c94e388102d441536ca094b782b72f51d16a
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:15:47 +0000

    3d9ca2f fix(compiler-cli): bind switch exhaustive check expressions

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-32b348a
+ * @license Angular v22.0.0+sha-3d9ca2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0+sha-32b348a",
+    version: "22.0.0+sha-3d9ca2f",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0+sha-32b348a",
+    version: "22.0.0+sha-3d9ca2f",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0+sha-32b348a",
+  version: "22.0.0+sha-3d9ca2f",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
