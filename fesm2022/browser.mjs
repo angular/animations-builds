56c57ffab533ff9d82214cdf9dc99380b0fe922c
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:29:37 +0000

    86a56dc fix(common): Limits date format string length

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.16+sha-b9d2938
+ * @license Angular v21.2.16+sha-86a56dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.16+sha-b9d2938",
+    version: "21.2.16+sha-86a56dc",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.16+sha-b9d2938",
+    version: "21.2.16+sha-86a56dc",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.16+sha-b9d2938",
+  version: "21.2.16+sha-86a56dc",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
