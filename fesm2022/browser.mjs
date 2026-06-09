51cd9f04f6071aa5936f94a256eddf9472ababaf
GitHub <noreply@github.com>
2026-06-09 17:12:21 +0000

    13fb0af refactor(platform-server): deprecate ServerXhr (#69255)

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.16+sha-86a56dc
+ * @license Angular v21.2.16+sha-13fb0af
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.16+sha-86a56dc",
+    version: "21.2.16+sha-13fb0af",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.16+sha-86a56dc",
+    version: "21.2.16+sha-13fb0af",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.16+sha-86a56dc",
+  version: "21.2.16+sha-13fb0af",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
