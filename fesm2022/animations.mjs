77a4e2ee9dfcf08c6d57211156bb79ffa1045a2a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-10 22:55:45 +0000

    4d0091b build: update cross-repo angular dependencies to v22.0.1

--- fesm2022/animations.mjs
+++ fesm2022/animations.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-8b32559
+ * @license Angular v22.0.1+sha-4d0091b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { inject, ANIMATION_MODULE_TYPE, ViewEncapsulation, ɵRuntimeError as _Ru
 class AnimationBuilder {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-8b32559",
+    version: "22.0.1+sha-4d0091b",
     ngImport: i0,
     type: AnimationBuilder,
     deps: [],
@@ -20,7 +20,7 @@ class AnimationBuilder {
   });
   static ɵprov = i0.ɵɵngDeclareService({
     minVersion: "22.0.0",
-    version: "22.0.1+sha-8b32559",
+    version: "22.0.1+sha-4d0091b",
     ngImport: i0,
     type: AnimationBuilder,
     factory: () => inject(BrowserAnimationBuilder)
@@ -28,7 +28,7 @@ class AnimationBuilder {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.1+sha-8b32559",
+  version: "22.0.1+sha-4d0091b",
   ngImport: i0,
   type: AnimationBuilder,
   decorators: [{
@@ -69,7 +69,7 @@ class BrowserAnimationBuilder extends AnimationBuilder {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-8b32559",
+    version: "22.0.1+sha-4d0091b",
     ngImport: i0,
     type: BrowserAnimationBuilder,
     deps: [{
@@ -81,7 +81,7 @@ class BrowserAnimationBuilder extends AnimationBuilder {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-8b32559",
+    version: "22.0.1+sha-4d0091b",
     ngImport: i0,
     type: BrowserAnimationBuilder,
     providedIn: 'root'
@@ -89,7 +89,7 @@ class BrowserAnimationBuilder extends AnimationBuilder {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.1+sha-8b32559",
+  version: "22.0.1+sha-4d0091b",
   ngImport: i0,
   type: BrowserAnimationBuilder,
   decorators: [{
