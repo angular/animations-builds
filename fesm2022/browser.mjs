f3c3b6e1256c4a4742fc39d611da734566ec78ad
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 17:22:54 +0000

    1d97355 docs: clarify value attribute on radio/checkbox inputs is allowed with formField

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-15314c1
+ * @license Angular v22.0.1+sha-1d97355
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-15314c1",
+    version: "22.0.1+sha-1d97355",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-15314c1",
+    version: "22.0.1+sha-1d97355",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.1+sha-15314c1",
+  version: "22.0.1+sha-1d97355",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
