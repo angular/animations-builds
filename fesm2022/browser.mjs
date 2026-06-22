154afa114f93a1faac3ce591cabcf3454ae9a0f2
kirjs <kirts@google.com>
2026-06-22 20:35:41 +0000

    59ee51e docs(forms): update package docs for Signal Forms graduation

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-a477a69
+ * @license Angular v22.0.2+sha-59ee51e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.2+sha-a477a69",
+    version: "22.0.2+sha-59ee51e",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.2+sha-a477a69",
+    version: "22.0.2+sha-59ee51e",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.2+sha-a477a69",
+  version: "22.0.2+sha-59ee51e",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
