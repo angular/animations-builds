f7fca65fd301f286d3a2760e3e2909f2a7765df2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 16:02:46 +0000

    eb10e93 refactor(common): modernize pipes & non bindable tests to rely on `whenStable`

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-644455b
+ * @license Angular v22.1.0+sha-eb10e93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0+sha-644455b",
+    version: "22.1.0+sha-eb10e93",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0+sha-644455b",
+    version: "22.1.0+sha-eb10e93",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0+sha-644455b",
+  version: "22.1.0+sha-eb10e93",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
