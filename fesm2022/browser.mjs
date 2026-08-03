0c95cb3193a701f759d1bd9b50f5ec6a59bb7783
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-03 16:04:04 +0000

    f2f1292 docs: fix roadmap link text and preview-site link

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-1b13db7
+ * @license Angular v22.2.0-next.0+sha-f2f1292
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.0+sha-1b13db7",
+    version: "22.2.0-next.0+sha-f2f1292",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.0+sha-1b13db7",
+    version: "22.2.0-next.0+sha-f2f1292",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.0+sha-1b13db7",
+  version: "22.2.0-next.0+sha-f2f1292",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
