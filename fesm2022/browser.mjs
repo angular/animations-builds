8d25dbdd9abfdbe6ddecc1c0d916658d1213a8a2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-19 00:15:26 +0000

    c819880 fix(forms): report forbidden 2way bindings on when `FormField` is applied

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-53fc371
+ * @license Angular v22.2.0-next.2+sha-c819880
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.2+sha-53fc371",
+    version: "22.2.0-next.2+sha-c819880",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.2+sha-53fc371",
+    version: "22.2.0-next.2+sha-c819880",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.2+sha-53fc371",
+  version: "22.2.0-next.2+sha-c819880",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
