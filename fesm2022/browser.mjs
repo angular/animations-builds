fca89997ea1cfdd551904e54e926d2013b6fe3ca
leonsenft <leonsenft@users.noreply.github.com>
2026-08-21 19:43:43 +0000

    64e02d8 docs: point the LLM indexes at current pages, not redirects

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.3+sha-ee63c0a
+ * @license Angular v22.1.3+sha-64e02d8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.3+sha-ee63c0a",
+    version: "22.1.3+sha-64e02d8",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.3+sha-ee63c0a",
+    version: "22.1.3+sha-64e02d8",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.3+sha-ee63c0a",
+  version: "22.1.3+sha-64e02d8",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
