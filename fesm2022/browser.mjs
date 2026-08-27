4de74ff64f0be86b4794c9c1701e5a52ab47f4e8
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-27 00:15:27 +0000

    8983809 release: cut the v22.1.4 release

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.3+sha-808f65f
+ * @license Angular v22.1.4+sha-8983809
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.3+sha-808f65f",
+    version: "22.1.4+sha-8983809",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.3+sha-808f65f",
+    version: "22.1.4+sha-8983809",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.3+sha-808f65f",
+  version: "22.1.4+sha-8983809",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
