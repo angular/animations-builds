c84632a6dbf96114295b3fe07649d8ac82b76d46
Matthew Beck <me@mattrb.com>
2026-08-27 15:08:54 +0000

    c2d4070 build: update all non-major dependencies

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-93ca7ab
+ * @license Angular v22.2.0-next.4+sha-c2d4070
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.4+sha-93ca7ab",
+    version: "22.2.0-next.4+sha-c2d4070",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.4+sha-93ca7ab",
+    version: "22.2.0-next.4+sha-c2d4070",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.4+sha-93ca7ab",
+  version: "22.2.0-next.4+sha-c2d4070",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
