d48247887a86056f636d624d951d40aa8d973d80
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:13:39 +0000

    dfe7be4 refactor(compiler): emit any as type argument for ɵɵInjectableDeclaration

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.5+sha-468b65b
+ * @license Angular v22.1.5+sha-dfe7be4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.5+sha-468b65b",
+    version: "22.1.5+sha-dfe7be4",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.5+sha-468b65b",
+    version: "22.1.5+sha-dfe7be4",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.5+sha-468b65b",
+  version: "22.1.5+sha-dfe7be4",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
--- types/_animation_driver-chunk.d.ts
+++ types/_animation_driver-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.5+sha-468b65b
+ * @license Angular v22.1.5+sha-dfe7be4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -42,7 +42,7 @@ declare class NoopAnimationDriver implements AnimationDriver {
      */
     animate(element: any, keyframes: Array<Map<string, string | number>>, duration: number, delay: number, easing: string, previousPlayers?: any[], scrubberAccessRequested?: boolean): AnimationPlayer;
     static ɵfac: i0.ɵɵFactoryDeclaration<NoopAnimationDriver, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<NoopAnimationDriver>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<any>;
 }
 /**
  * @publicApi
