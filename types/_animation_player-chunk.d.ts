628fc660d4f43efe2fd013e3d6c665c95ab0b49d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 14:44:58 +0000

    2c71e88 docs: update cross-repo adev docs

--- types/_animation_player-chunk.d.ts
+++ types/_animation_player-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.5+sha-5afdd98
+ * @license Angular v22.2.0-next.5+sha-2c71e88-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1159,7 +1159,7 @@ declare function useAnimation(animation: AnimationReferenceMetadata, options?: A
  * Tokens can be merged into a combined query selector string. For example:
  *
  * ```ts
- *  query(':self, .record:enter, .record:leave, @subTrigger', [...])
+ *  query('@subTrigger, :self, .record:enter, .record:leave', [...])
  * ```
  *
  * The `query()` function collects multiple elements and works internally by using
