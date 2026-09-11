1384b940d04b49ce7c038b31ffdc89ee37f67e79
Andrew Scott <atscott01@gmail.com>
2026-09-11 23:07:50 +0000

    fc2f6fd docs: stop the disabled aria toolbar radio group from changing selection

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-f2cf65a
+ * @license Angular v22.2.0-next.7+sha-fc2f6fd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-f2cf65a",
+    version: "22.2.0-next.7+sha-fc2f6fd",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-f2cf65a",
+    version: "22.2.0-next.7+sha-fc2f6fd",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.7+sha-f2cf65a",
+  version: "22.2.0-next.7+sha-fc2f6fd",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
