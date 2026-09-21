6b028e65258c5c08ceb9d46204674ed9a96e3961
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 18:56:47 +0000

    0ee60b4 build: bump pnpm yaml file

--- fesm2022/_private_export-chunk.mjs
+++ fesm2022/_private_export-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-b0d4010-with-local-changes
+ * @license Angular v22.3.0-next.0+sha-0ee60b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
