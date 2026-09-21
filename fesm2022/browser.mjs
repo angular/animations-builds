f25c956f6b9529d78dd75dbe884400d532523f74
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 21:49:35 +0000

    d5f409c docs: sync AI context files with best-practices.md

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-5cd618f
+ * @license Angular v22.3.0-next.0+sha-d5f409c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-5cd618f",
+    version: "22.3.0-next.0+sha-d5f409c",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-5cd618f",
+    version: "22.3.0-next.0+sha-d5f409c",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.3.0-next.0+sha-5cd618f",
+  version: "22.3.0-next.0+sha-d5f409c",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
