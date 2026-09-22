1ed64a8fc2ee40da7834988d940df12ec686e209
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 16:23:13 +0000

    d7d5401 fix(core): don't use plain objects as maps for jsaction data

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-8db8817
+ * @license Angular v22.1.7+sha-d7d5401
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.7+sha-8db8817",
+    version: "22.1.7+sha-d7d5401",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.7+sha-8db8817",
+    version: "22.1.7+sha-d7d5401",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.7+sha-8db8817",
+  version: "22.1.7+sha-d7d5401",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
