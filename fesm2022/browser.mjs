3b4cec48357efcb99412dba636171f1aaed14987
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:52:34 +0000

    123729b docs: release notes for the v21.2.24 release

--- fesm2022/browser.mjs
+++ fesm2022/browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-42ce791
+ * @license Angular v22.3.0-next.0+sha-123729b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -30,7 +30,7 @@ class NoopAnimationDriver {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-42ce791",
+    version: "22.3.0-next.0+sha-123729b",
     ngImport: i0,
     type: NoopAnimationDriver,
     deps: [],
@@ -38,14 +38,14 @@ class NoopAnimationDriver {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-42ce791",
+    version: "22.3.0-next.0+sha-123729b",
     ngImport: i0,
     type: NoopAnimationDriver
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.3.0-next.0+sha-42ce791",
+  version: "22.3.0-next.0+sha-123729b",
   ngImport: i0,
   type: NoopAnimationDriver,
   decorators: [{
